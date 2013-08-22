7d1c199b2e5e1d6d99359e29ce3527a6d204b8ec
Boubacar KOITA <boubacar.koita@gmail.com>
2013-08-22 18:36:03 +0200

    Ajout template page statistique investir

---
+++ page-investir.php
@@ -0,0 +1,61 @@
+<?php get_header(); ?>
+
+<?php 
+    date_default_timezone_set("Europe/Paris");
+    require_once("common.php");
+    
+    global $campaign, $post;
+    if ( ! is_object( $campaign ) )
+	    $campaign = atcf_get_campaign( $post );
+?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div id="content">
+		<div class="padder">
+
+			<?php do_action( 'bp_before_blog_single_post' ); ?>
+
+			<div class="page" id="blog-single" role="main">
+				<?php 
+				    printAdminBar();
+				?>
+			    
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				    <div class="post-content">
+					<div class="entry">
+					    <?php printPageTop($post); ?>
+					    
+					    <?php printPageBottomStart($post, $campaign); ?>
+					    
+					    <div><?php echo html_entity_decode($campaign->summary()); ?></div>
+
+					   
+					    <span><?php the_content(); ?></span>
+
+					
+					    </div>
+					    <?php 
+						 printPageBottomEnd($post, $campaign);
+						?>
+					</div>
+				    </div>
+				</div>
+
+			</div>
+			
+
+			<?php do_action( 'bp_after_blog_single_post' ); ?>
+
+		</div><!-- .padder -->
+	</div><!-- #content -->
+
+
+<?php endwhile; else: ?>
+	<div id="content">
+	    <div class="padder center">
+		<p><?php _e( 'Sorry, no posts matched your criteria.', 'buddypress' ); ?></p>
+	    </div><!-- .padder -->
+	</div><!-- #content -->
+<?php endif; ?>
+	
+<?php get_footer(); ?>
