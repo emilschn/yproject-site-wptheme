e872430c873fd327befd4d10cde239db38dfcde2
Emilien Schneider <emilien.schneider@gmail.com>
2013-12-23 16:36:35 +0100

    Mise en page footer et connexion Signed-off-by: Emilien Schneider <emilien.schneider@gmail.com>

--- footer.php
+++ footer.php
@@ -5,21 +5,20 @@
 
 		<footer>
 		    <div class="center">
-			<span>Plateforme d&apos;investissement participatif &agrave; impact positif.</span>
-			
 			<?php if ( is_active_sidebar( 'first-footer-widget-area' ) || is_active_sidebar( 'second-footer-widget-area' ) || is_active_sidebar( 'third-footer-widget-area' ) || is_active_sidebar( 'fourth-footer-widget-area' ) ) : ?>
 				<?php get_sidebar( 'footer' ); ?>
 			<?php endif; ?>
 
 			<div id="site-generator" role="contentinfo" style="text-align:center; color: white; font-size:12px;">
 			    <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/fr/">
-				<img alt="Licence Creative Commons" width="25" height="25" border="0" style="vertical-align: middle" src="<?php echo get_stylesheet_directory_uri(); ?>/images/cc_logo.png" /> SOME RIGHTS RESERVED
+				<img alt="Licence Creative Commons" width="25" height="25" border="0" style="vertical-align: middle" src="<?php echo get_stylesheet_directory_uri(); ?>/images/cc_logo.png" /> Some rights reserved
 			    </a>
+			    <span>Plateforme d&apos;investissement participatif &agrave; impact positif.</span>
 			</div>
 
 			<?php do_action( 'bp_footer' ); ?>
 			
-		    </div>
+		     </div>
 		</footer>
 
 		<?php do_action( 'bp_after_footer' ); ?>
