851e016747ce105e8d61fdcf4909b46938e4b569
faouzi <faouzi@wedogood.co>
2014-04-15 15:25:32 +0200

    modification de template-basic pour mentions legales

--- template-basic.php
+++ template-basic.php
@@ -4,29 +4,36 @@
  * Template Name: Template Basique
  *
  */
-  get_header(); ?>
+get_header(); ?>
 
 <?php 
-    date_default_timezone_set("Europe/Paris");
+date_default_timezone_set("Europe/Paris");
 ?>
+<div id="content">
+	<div class="padder">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<div id="content">
-		<div class="padder">
-		    <?php locate_template( array("basic/basic-header.php"), true ); ?>
-		    <div id="post_bottom_bg">
+		<?php locate_template( array("basic/basic-header.php"), true ); ?>
+		<div id="post_bottom_bg">
 			<div id="post_bottom_content" class="center">
-			    <?php the_content(); ?>
+				<?php 
+				$page_name = get_post($post)->post_name;
+				if ($page_name=="mentions-legales") {?>
+				<div class="left post_bottom_desc_small">
+					<?php } ?>
+					<?php the_content(); ?>
+					<?php if ($page_name=="mentions-legales") {?>
+					<br />
+					<!-- GeoTrust QuickSSL [tm] Smart Icon tag. Do not edit. -->
+					<center><SCRIPT LANGUAGE="JavaScript" TYPE="text/javascript" SRC="https://smarticon.geotrust.com/si.js"></SCRIPT></center>
+					<!-- end GeoTrust Smart Icon tag -->
+					<br />
+				</div>
+				<?php } ?>
+				<div style="clear: both"></div>
 			</div>
-		    </div> 
-		</div><!-- .padder -->
-	</div><!-- #content -->
-
-<?php endwhile; else: ?>
-	<div id="content">
-	    <div class="padder center">
-		<p><?php _e( 'Sorry, no posts matched your criteria.', 'buddypress' ); ?></p>
-	    </div><!-- .padder -->
-	</div><!-- #content -->
-<?php endif; ?>
+		</div>
+	<?php endwhile; endif; ?>
+</div><!-- .padder -->
+</div><!-- #content -->
 <?php get_footer(); ?>
