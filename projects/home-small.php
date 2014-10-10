cf14dfa06250bba8fe3d651192fe3757d38545bb
Emilien Schneider <emilien.schneider@gmail.com>
2014-10-10 10:28:53 +0200

    Refonte petits projets page accueil Signed-off-by: Emilien Schneider <emilien.schneider@gmail.com>

--- projects/home-small.php
+++ projects/home-small.php
@@ -1,30 +1,37 @@
 <?php
-function print_vote_post($vote_post,$is_right_project){
+function print_vote_post($vote_post, $is_right_project){
 	global $post;
 	$temp_post = $post;
 	$post = $vote_post;
 	$campaign = atcf_get_campaign( $vote_post );
+	$position_str = ($is_right_project) ? 'right' : 'left';
+	$is_right_project = !$is_right_project;
         ?>
-	<div class="home-small-project status-vote <?php if ($is_right_project){echo "home-small-project-right";$is_right_project=false;}else{ echo "home-small-project-left"; $is_right_project=true;}?>">
+	<div class="home-small-project status-vote <?php echo "home-small-project-" . $position_str; ?>">
+	    
 		<h2><a href="<?php echo get_permalink($vote_post->ID); ?>"><?php echo $vote_post->post_title; ?></a></h2>
+		
 		<div class="description-separator first-description-separator"></div>
 		
-			<a href="<?php echo get_permalink($vote_post->ID); ?>">
-				<?php if ($is_right_project) { ?>
-				<div class="vote-bubble-left" >
-				<?php } else { ?>
-				<div class="vote-bubble-right">
-				<?php } ?>
+		<a href="<?php echo get_permalink($vote_post->ID); ?>">
+			<div class="vote-bubble-<?php echo $position_str; ?>" >
+				    
+				<?php if ($campaign->end_vote_remaining() > 0) : ?>
 					<p>Projet</p>
 					<p class="big-text">En vote</p>
 					<p class="small-text">jusqu'au <?php echo $campaign->end_vote_date_home() ?> </p>
-				</div>
-			</a>
-
+				<?php else: ?>
+					<p style="margin-top: 5px;">Projet</p>
+					<p class="big-text">valid&eacute;</p>
+				<?php endif; ?>
+					
+			</div>
+		</a>
+		
 		<div class="description-zone">
-			<div class="description-summary ">
-				<a href="<?php echo get_permalink($vote_post->ID);?>">
-				<?php $content =$campaign->summary();echo $content;?>
+			<div class="description-summary">
+				<a href="<?php echo get_permalink($vote_post->ID);?>" style="height: 75px;">
+				<?php echo $campaign->summary();?>
 				</a>
 			</div>
 		    
@@ -34,60 +41,42 @@ function print_vote_post($vote_post,$is_right_project){
 					<?php 
 					$campaign_location = $campaign->location();
 					$exploded = explode(' ', $campaign_location);
-					if (count($exploded) > 1) $campaign_location = $exploded[0];
-					echo (($campaign_location != '') ? $campaign_location : 'France'); 
+					$campaign_location_str = (count($exploded) > 1) ? $exploded[0] : 'France';
+					echo $campaign_location_str; 
 					?>
 				</div>
 				<div class="description-logos-item" style="width: 45px;">
 					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/good.png" alt="Logo J'y crois" /><br />
 					<?php echo $campaign->get_jycrois_nb(); ?>
+
 				</div>
 				<div class="description-logos-item">
-				    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/horloge.png" alt="Logo Horloge" /><br />
-				    <?php echo $campaign->end_vote_remaining(); ?>
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/horloge.png" alt="Logo Horloge" /><br />
+					<?php echo $campaign->end_vote_remaining(); ?>
 				</div>
 			</div>
 
 		</div>
-		<?php 
-		$video_element = '';
-		$img_src = '';
-		if ($campaign->video() == '') {
-			$attachments = get_posts( array(
-				'post_type' => 'attachment',
-				'post_parent' => $vote_post->ID,
-				'post_mime_type' => 'image'
-				));
-			$image_obj = '';
-			//Si on en trouve bien une avec le titre "image_home" on prend celle-là
-			foreach ($attachments as $attachment) {
-				if ($attachment->post_title == 'image_home') $image_obj = wp_get_attachment_image_src($attachment->ID, "full");
-			}
-			//Sinon on prend la première image rattachée à l'article
-			if ($image_obj == '') $image_obj = wp_get_attachment_image_src($attachments[0]->ID, "full");
-			if ($image_obj != '') $img_src = $image_obj[0];
-			
-		//Sinon on utilise l'objet vidéo fourni par wordpress
-		} else {
-			$video_element = wp_oembed_get($campaign->video(), array('width' => 440));
-		}
-		?>
+		
+		<?php $img_src = $campaign->get_home_picture_src(); ?>
 		
 		<?php if ($img_src != ''): ?>
 		<a href="<?php the_permalink(); ?>" style="display: block;">
-		<?php endif; ?>
-		<div class="video-zone" <?php if ($img_src != '') { ?>style="background-image: url('<?php echo $img_src; ?>');clear: both;"<?php } ?> >
-			<?php echo $video_element;
-			if ($video_element == '') { ?>
+			<div class="video-zone" style="background-image: url('<?php echo $img_src; ?>'); clear: both;">
+				<?php if ($campaign->end_vote_remaining() > 0) : ?>
 				<div class="vote-banner"></div>
-			<?php } ?>
-		</div>
-		<?php if ($img_src != ''): ?>
+				<?php endif; ?>
+			</div>
 		</a>
 		<?php endif; ?>
 		
-		<div class="description-separator " <?php if($video_element!='')echo "style='margin-top: 95px;'";?>></div>
+		<div class="description-separator"></div>
+		
+		<?php if ($campaign->end_vote_remaining() > 0) : ?>
 		<a href="<?php echo get_permalink($vote_post->ID); ?>" class="description-discover"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_droite.png" alt="triangle"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_droite.png" alt="triangle">Voter sur ce projet ici<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_gauche.png" alt="triangle"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_gauche.png" alt="triangle"></a>
+		<?php else: ?>
+		<a href="<?php echo get_permalink($vote_post->ID); ?>" class="description-discover"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_droite.png" alt="triangle"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_droite.png" alt="triangle">Voir le projet<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_gauche.png" alt="triangle"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_gauche.png" alt="triangle"></a>
+		<?php endif; ?>
 	</div>
 
 <?php
@@ -102,21 +91,25 @@ function print_vote_post($vote_post,$is_right_project){
 
 		
 <?php
- function print_preview_post($preview_post,$is_right_project){
+ function print_preview_post($preview_post, $is_right_project) {
 	global $post;
 	$temp_post = $post;
 	$post = $preview_post;
 	$campaign = atcf_get_campaign( $preview_post );
+	$position_str = ($is_right_project) ? 'right' : 'left';
+	$is_right_project = !$is_right_project;
 	?>
-
-<div class="home-small-project status-preview<?php echo $container_class; ?> <?php if ($is_right_project){echo "home-small-project-right";$is_right_project=false;}else{ echo "home-small-project-left"; $is_right_project=true;}?>">
+	<div class="home-small-project status-preview home-small-project-<?php echo $position_str; ?>">
 
 		<h2><a href="<?php echo get_permalink($preview_post->ID) ?>"><?php echo $preview_post->post_title; ?></a></h2>
+		
 		<div class="description-separator first-description-separator"></div>
+		
 		<div class="description-zone">
+		    
 			<div class="description-summary">
 				<a href="<?php echo get_permalink($preview_post->ID);?>">
-				<?php $content =html_entity_decode($campaign->summary()); echo $content; ?> 
+					<?php echo html_entity_decode($campaign->summary()); ?> 
 				</a>
 			</div>
 		    
@@ -126,8 +119,8 @@ function print_vote_post($vote_post,$is_right_project){
 					<?php 
 					$campaign_location = $campaign->location();
 					$exploded = explode(' ', $campaign_location);
-					if (count($exploded) > 1) $campaign_location = $exploded[0];
-					echo (($campaign_location != '') ? $campaign_location : 'France'); 
+					$campaign_location_str = (count($exploded) > 1) ? $exploded[0] : 'France';
+					echo $campaign_location_str; 
 					?>
 				</div>
 				<div class="description-logos-item" style="width: 45px;">
@@ -136,51 +129,24 @@ function print_vote_post($vote_post,$is_right_project){
 				</div>
 			</div>
 		</div>
-		<?php 
-		$video_element = '';
-		$img_src = '';
-		//Si aucune vidéo n'est définie, ou si on est encore en mode preview, on affiche l'image
-		if ($campaign->video() == '') {
-			$attachments = get_posts( array(
-							    'post_type' => 'attachment',
-							    'post_parent' => $preview_post->ID,
-							    'post_mime_type' => 'image'
-					));
-			$image_obj = '';
-			//Si on en trouve bien une avec le titre "image_home" on prend celle-là
-			foreach ($attachments as $attachment) {
-			    if ($attachment->post_title == 'image_home') $image_obj = wp_get_attachment_image_src($attachment->ID, "full");
-			}
-			//Sinon on prend la première image rattachée à l'article
-			if ($image_obj == '') $image_obj = wp_get_attachment_image_src($attachments[0]->ID, "full");
-			if ($image_obj != '') $img_src = $image_obj[0];
-			
-		//Sinon on utilise l'objet vidéo fourni par wordpress
-		} else {
-			$video_element = wp_oembed_get($campaign->video(), array('width' => 440));
-		}
-		?>
-		<div class="video-zone" <?php if ($img_src != '') { ?>style="background-image: url('<?php echo $img_src; ?>')"<?php } ?> >
-			<?php echo $video_element;
-			if ($video_element == '') { ?>
-					<div class="preview-banner"></div>
-				<?php }
-			 ?>
+		
+		<?php $img_src = $campaign->get_home_picture_src(); ?>
+		
+		<?php if ($img_src != ''): ?>
+		<div class="video-zone" style="background-image: url('<?php echo $img_src; ?>')">
+			<div class="preview-banner"></div>
 			<a href="<?php echo get_permalink($preview_post->ID); ?>">
-				<?php
-				if($is_right_project){//si a gauche
-				?>
-					<div class="preview-bubble-left">
-				<?php } else { ?>
-					<div class="preview-bubble-right">	
-				<?php } ?>
-				<p>Projet</p>
-				<p>En avant</p>
-				<p>Premiere</p>
+				<div class="preview-bubble-<?php echo $position_str; ?>">
+					<p>Projet</p>
+					<p>En avant</p>
+					<p>Premiere</p>
 				</div>
 			</a>
 		</div>
+		<?php endif; ?>
+			    
 		<div class="description-separator"></div>
+		
 		<a href="<?php echo get_permalink($preview_post->ID); ?>" class="description-discover"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_droite.png" alt="triangle"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_droite.png" alt="triangle">D&eacute;couvrir le projet ici<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_gauche.png" alt="triangle"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/triangle_blanc_vers_gauche.png" alt="triangle"></a>
 	</div>
 	
@@ -192,9 +158,9 @@ function print_vote_post($vote_post,$is_right_project){
 ?>
 
 <?php             
-function print_empty_post(){
-        $page_propose_project = get_page_by_path('proposer-un-projet');?>
-		<a href="<?php echo get_permalink($page_propose_project->ID); ?>">	
-        <div class="home-small-project home-small-project-right home-small-project-empty"></div>
+function print_empty_post() {
+        $page_propose_project = get_page_by_path('proposer-un-projet'); ?>
+	<a href="<?php echo get_permalink($page_propose_project->ID); ?>">	
+		<div class="home-small-project home-small-project-right home-small-project-empty"></div>
         </a>
 <?php } ?>
