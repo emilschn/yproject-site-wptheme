82f09663229e4cb7108bc21e7de6000255a54872
Emilien Schneider <emilien.schneider@gmail.com>
2014-11-13 09:30:44 +0100

    Ajout de boutons sur le tableau de bord Signed-off-by: Emilien Schneider <emilien.schneider@gmail.com>

--- template-project-dashboard.php
+++ template-project-dashboard.php
@@ -22,6 +22,11 @@ $campaign_id = $_GET['campaign_id'];
 					$campaign = atcf_get_campaign($post_campaign);
 					$page_guide = get_page_by_path('guide');
 					$page_particular_terms = get_page_by_path('conditions-particulieres');
+					
+					$category_slug = $post_campaign->ID . '-blog-' . $post_campaign->post_name;
+					$category_obj = get_category_by_slug($category_slug);
+					$category_link = (!empty($category_obj)) ? get_category_link($category_obj->cat_ID) : '';
+					$news_link = esc_url($category_link);
 					?>
 
 					<?php if ($can_modify): ?>
@@ -37,11 +42,16 @@ $campaign_id = $_GET['campaign_id'];
 						<div class="button-help">
 							<a href="<?php echo get_permalink($page_particular_terms->ID); ?>" target="_blank"><?php _e('Conditions particuli&egrave;res', 'yproject'); ?></a>
 							<a href="<?php echo get_permalink($page_guide->ID); ?>" target="_blank"><?php _e('Guide de campagne', 'yproject'); ?></a>
+							
+							<?php if ($campaign->google_doc() != ''): ?>
+							<a href="https://docs.google.com/spreadsheets/d/<?php echo $campaign->google_doc(); ?>/edit" target="_blank" class="button"><?php _e('Ouvrir le document de gestion de campagne', 'yproject'); ?></a>
+							<?php endif; ?>
+							<a href="<?php echo $news_link; ?>" class="button"><?php _e('Publier une actualit&eacute;', 'yproject'); ?></a>
+							
 							<div class="clear"></div>
 						</div>
 
 						<?php if ($campaign->google_doc() != ''): ?>
-						<a href="https://docs.google.com/spreadsheets/d/<?php echo $campaign->google_doc(); ?>/edit" target="_blank"><?php _e('Editer le document', 'yproject'); ?></a>
 						<div class="google-doc">
 							<iframe src="https://docs.google.com/spreadsheets/d/<?php echo $campaign->google_doc(); ?>/pubhtml?widget=true&amp;headers=false"></iframe>
 						</div>
