002d6df4060be1fc74639c833653fa96145835be
Emilien Schneider <emilien.schneider@gmail.com>
2015-02-06 11:16:54 +0100

    Accueil alégé + sélection des items dans le menu Signed-off-by: Emilien Schneider <emilien.schneider@gmail.com>

--- header.php
+++ header.php
@@ -45,6 +45,15 @@
 	</head>
 
 	<body <?php body_class(); ?> id="bp-default">
+	    
+		<?php 
+		$menu_pages = array(
+			'les-projets' => 'Les projets',
+			'proposer-un-projet' => 'Proposer un projet',
+			'descriptif' => 'Comment ca marche ?',
+			'blog' => 'Blog'
+		);
+		?>
 
 		<nav id="navigation" role="navigation">
 			<div class="center">
@@ -54,14 +63,16 @@
 						<img src="<?php echo $stylesheet_directory_uri; ?>/images/logo.png" width="160" height="100" alt="logo" />
 						<img src="<?php echo $stylesheet_directory_uri; ?>/images/logo_court.png" width="160" height="51" alt="logo court" style="display: none;" />
 					</a></li>
-					<?php /* Menu Les projets */ $page_projects = get_page_by_path('les-projets'); ?>
-					<li class="page_item"><span class="page_item_border"><a href="<?php echo get_permalink($page_projects->ID); ?>"><?php echo __('Les projets', 'yproject'); ?></a></span></li>
-					<?php /* Menu Proposer un projet */ $page_start = get_page_by_path('proposer-un-projet'); ?>
-					<li class="page_item"><span class="page_item_border"><a href="<?php echo get_permalink($page_start->ID); ?>"><?php echo __('Proposer un projet', 'yproject'); ?></a></span></li>
-					<?php /* Menu Comment ça marche ? */ $page_how = get_page_by_path('descriptif'); ?>
-					<li class="page_item"><span class="page_item_border"><a href="<?php echo get_permalink($page_how->ID); ?>"><?php echo __('Comment ca marche ?', 'yproject'); ?></a></span></li>
-					<?php /* Menu Blog */ $page_blog = get_page_by_path('blog'); ?>
-					<li class="page_item"><span class="page_item_border"><a href="<?php echo get_permalink($page_blog->ID); ?>"><?php echo __('Blog', 'yproject'); ?></a></span></li>
+				    
+					<?php global $post; ?>
+					<?php foreach ($menu_pages as $menu_page_key => $menu_page_label): ?>
+						<?php 
+							$menu_page_object = get_page_by_path($menu_page_key);
+							$selected = ($post->post_name == $menu_page_key) ? ' selected' : ''; 
+						?>
+						<li class="page_item<?php echo $selected; ?>"><span class="page_item_border"><a href="<?php echo get_permalink($menu_page_object->ID); ?>"><?php _e($menu_page_label, 'yproject'); ?></a></span></li>
+					<?php endforeach; ?>
+				    
 					<?php /* Logo FB / TW */ ?>
 					<li class="page_item_out mobile_hidden" id="menu_item_facebook"><a href="https://www.facebook.com/wedogood.co" target="_blank" title="Notre page Facebook"><img src="<?php echo $stylesheet_directory_uri; ?>/images/facebook.png" width="20" height="20" alt="logo facebook" /></a></li>
 					<li class="page_item_out mobile_hidden" id="menu_item_twitter"><a href="https://twitter.com/wedogood_co" target="_blank" title="Notre compte Twitter"><img src="<?php echo $stylesheet_directory_uri; ?>/images/twitter.png" width="20" height="20" alt="logo twitter" /></a></li>
--- page-home.php
+++ page-home.php
@@ -1,3 +1,5 @@
+<?php date_default_timezone_set("Europe/Paris"); ?>
+
 <?php if (is_user_logged_in() && isset($_GET['alreadyloggedin']) && $_GET['alreadyloggedin'] === '1'): ?>
 <div id="already-connected">
 	<div class="already-connected-padder">
@@ -10,16 +12,9 @@
 </div>
 <?php endif; ?>
 
-<?php 
-date_default_timezone_set("Europe/Paris");
-require_once('requests/projects.php'); 
-$cache_result=$WDG_cache_plugin->get_cache('home-top');
-if(false===$cache_result){
-	ob_start();
- ?>
-<header class="align-center header_home">
-	<section id="site_name2" class="center">
-		<div id="welcome_text">
+<header class="header_home">
+	<div class="center">
+		<section id="welcome_text">
 			<?php the_content();?>
 			<?php if ( is_user_logged_in() ) { ?>
 				<?php global $current_user; get_currentuserinfo(); ?>
@@ -32,311 +27,79 @@ if(false===$cache_result){
 				<a href="<?php echo get_permalink($page_connexion->ID); ?>" class="button">Connexion</a>
 			</div>
 			<?php } ?>
-		</div>
-	</section>
+		</section>
+	</div>
 </header>
 
-<?php
-	$cache_result=ob_get_contents();
-	$WDG_cache_plugin->set_cache('home-top',$cache_result,60*60*24);
-	ob_end_clean();
-	}
-	echo $cache_result;
-	?>
 
-<div id="content">
-	<?php 
-		$cache_result=$WDG_cache_plugin->get_cache('home-collecte-projects');
-		if(false===$cache_result){
-		    ob_start();
-		    $nb_collecte_projects=count(query_projects_collecte()); 
-		    if($nb_collecte_projects>0){?>
-			<div class="part-title-separator" >
-				<span class="part-title"> 
-					En cours de financement
-				</span>
-			</div>
-		    <?php } ?>
-
-		    <div id="home_top" class="center">
-			    <div class="padder">
-				    <?php require('projects/home-large.php'); ?>
-			    </div>
-		    </div>
-		<?php 
-		    $cache_result=ob_get_contents();
-		    $WDG_cache_plugin->set_cache('home-collecte-projects',$cache_result,2*60*60);
-		    ob_end_clean();
-		}
-		echo $cache_result; 
-		
-		$cache_result=$WDG_cache_plugin->get_cache('home-small-projects');
-		if(false===$cache_result){
-		    ob_start();
-		    require('projects/home-small.php');
-		    $is_right_project=true;
-		    $preview_projects=query_projects_preview();
-		    $vote_projects = query_projects_vote();
-		    $nb_vote_projects=count($vote_projects);
-		    $nb_preview_projects=count($preview_projects);
-		    $nb_total_projects=$nb_vote_projects+$nb_preview_projects;
-		    if($nb_total_projects>0){
-				?>
-                	<div class="part-title-separator">
-                        <span class="part-title"> 
-                            Prochainement
-						</span>
-                    </div>
-                    <div class="center">
-			<div class="padder">
-			    <?php
-				$nb_printed_post=0;
-				$is_last_post=false;
-				if ($nb_vote_projects>0) {
-				    foreach ($vote_projects as $vote_post) {
-					$nb_printed_post++;
-					if($nb_printed_post==$nb_total_projects&&$nb_total_projects%2!=0){
-					    $is_right_project=false;
-					    $is_last_post=true;
-					}
-					$is_right_project=print_vote_post($vote_post,$is_right_project);
-				    }
-				    if($is_last_post) print_empty_post();
-				}
-				if ($nb_preview_projects>0) {
-				    foreach ($preview_projects as $preview_post) {
-					$nb_printed_post++;
-					if($nb_printed_post==$nb_total_projects&&$nb_total_projects%2!=0){
-					    $is_right_project=false;
-					    $is_last_post=true;
-					}
-					$is_right_project=print_preview_post($preview_post,$is_right_project);
-				    }
-				    if($is_last_post) print_empty_post();
-				}
-			    ?>
-                     	</div>
-                    </div>
-		    <?php
-		    }
-		    $cache_result=ob_get_contents();
-		    $WDG_cache_plugin->set_cache('home-small-projects',$cache_result,2*60*60);
-		    ob_end_clean();
-		}
-		echo $cache_result;
-
-			$cache_result=$WDG_cache_plugin->get_cache('home-funded-projects');
-			if(false===$cache_result){
-			ob_start();
-			  ?>
-			<div  class="center">
-			 	<div class="part-title-separator">
-					<?php
-	 			  			$nb_funded_projects=count(query_projects_funded()); 
-	 			  			if($nb_funded_projects>0){?>
-	 			  				<span class="part-title" > 
-                           			 D&#201;j&agrave; financ&#201;
-								</span>	
-	 			 	<?php } ?>
-   				</div>
-   				<?php 	
-   					if($nb_funded_projects>0){
-   						require('projects/home-large.php'); ?>
-   						<div class="part-title-separator"></div>
-   				<?php } ?>
-    	 	</div>
-    	 	<?php 
-    	 	$cache_result=ob_get_contents();
-    	 	$WDG_cache_plugin->set_cache('home-funded-projects',$cache_result,2*60*60);
-				ob_end_clean();
-			}
-			echo $cache_result;
-		?>
-    
-	<div id="home_middle">
-		<div id="home_middle_top">
-			<div id="home_middle_content">
-				<div class="center">
-					<?php $url = 'https://www.wedogood.co/campaigns/la-ferme-de-milgoulle'; ?>
-					<a href="<?php echo $url; ?>" style="display: block;">
-						<div class="round_title_left">
-							<strong>Participez</strong><br/>&agrave; un projet
-						</div>
-					</a>
-					<a href="<?php echo get_permalink($page_new_project->ID); ?>" style="display: block;">
-						<div class="round_title_right">
-							<strong>Proposez</strong><br />un projet
-						</div>
-					</a>
-					<div class="clear"></div>
-				</div>
-			</div>
-		</div>
-		<div class="home_middle_desc">
-			<div id="home_middle_desc_left">
-				<p>Soyez acteurs et influenceurs</p>
-				<p>de la communaut&eacute;</p>
-				<img src="<?php echo $stylesheet_directory_uri; ?>/images/main.jpg" alt="logo main"/>
-				<p>Votez pour les projets les plus impactants</p>
-				<img src="<?php echo $stylesheet_directory_uri; ?>/images/pieces.jpg" alt="logo piece monnaie"/>
-				<p>Investissez &agrave; partir de 10&euro;</p>
-				<img src="<?php echo $stylesheet_directory_uri; ?>/images/fusee.jpg" alt="logo fusee"/>
-				<p>Boostez l&apos;&eacute;conomie positive</p>
-			</div>
-			<div id="home_middle_desc_right">
-				<p>B&eacute;n&eacute;ficiez d'un financement souple</p>
-				<p>et adapt&eacute; &agrave; vos besoins.</p>
-				<img src="<?php echo $stylesheet_directory_uri; ?>/images/sous.jpg" alt="logo euro"/>
-				<p>Trouvez un financement pour votre projet</p>
-				<img src="<?php echo $stylesheet_directory_uri; ?>/images/hp.jpg" alt="logo megaphone"/>
-				<p>Faites conna&icirc;tre votre projet</p>
-				<img src="<?php echo $stylesheet_directory_uri; ?>/images/communaute.jpg" alt="logo communaute"/>
-				<p>F&eacute;d&eacute;rez une communaut&eacute; sur la dur&eacute;e</p>
-			</div>
-		</div>
-	</div>
-    
-	<div id="home_bottom" class="center">
-		<div class="padder">
-	   		<div class="part-title-separator">
-				<span class="part-title"> 
-					Actualit&#201;s
-				</span>
-			</div>
-			<div class="home-activity-list-container">
-					<ul class="home-activity-list">
-						<?php // Affichage du fil d'actualité
-							if ( bp_has_activities( bp_ajax_querystring( 'activity' ).'&max=10' ) ) :
-							date_default_timezone_set('Atlantic/Azores');
-							while ( bp_activities() ) : bp_the_activity();
-									locate_template( array( 'activity/entry.php' ), true, false );
-							endwhile;
-							date_default_timezone_set("Europe/London");
-							endif; 
-						?>
-					</ul>
-			</div>
-			<div class="home-blog-list-container">
-					<ul class="home-blog-list">
-						<?php 	$cache_result=$WDG_cache_plugin->get_cache('home-blog');
-								if(false===$cache_result){
-								ob_start(); 
-							$nb_posts = 3;
-							query_posts( array(
-								'post_status' => 'publish',
-								'category_name' => 'wedogood',
-								'orderby' => 'post_date',
-								'order' => 'desc',
-								'showposts' => $nb_posts
-							) );
-						if ( have_posts() ) :
-							while (have_posts()) : the_post(); 
-								wdg_showblogitem();
-							endwhile;
-					endif;
-					$cache_result=ob_get_contents();
-					$WDG_cache_plugin->set_cache('home-blog',$cache_result,2*60*60);
-						ob_end_clean();
-						}
-						echo $cache_result;  ?>
-					<div style="clear: both;">
-					</div>
-					</ul>
-					<div class="home-blog-list-nav">
-					<?php for ($i = 1; $i <= 3; $i++) { ?>
-						<a href="javascript:void(0);" class="home-blog-btn<?php if($i == 1) echo ' selected'; ?>" data-targetitem="<?php echo ($i-1); ?>"><?php echo $i; ?></a>
-					<?php } ?>
-					</div>
-					<div class="home-blog-list-more">
-					<?php $page_blog = get_page_by_path('blog');?>
-					<a href="<?php echo get_permalink($page_blog->ID); ?>">
-						Tout le blog
-					</a>
-					</div>
-			</div>
-				<div class="home-news-list-container">
-					<ul class="home-news-list">
-						<?php 
-						$cache_result=$WDG_cache_plugin->get_cache('home-news');
-						if(false===$cache_result){
-							ob_start(); 
-							query_posts(array(
-								'post_status' => 'publish',
-								'category_name' => 'revue-de-presse',
-								'orderby' => 'post_date',
-								'order' => 'desc',
-								'showposts' => 5
-							) );
-							if ( have_posts() ) :
-								while (have_posts()) : the_post(); 
-									wdg_shownewsitem();
-								endwhile; 
-							endif;
-							$cache_result=ob_get_contents();
-							$WDG_cache_plugin->set_cache('home-news',$cache_result,2*60*60);
-							ob_end_clean();
-						}
-						echo $cache_result;
-						?>
-						<li class="clear"></li>
-					</ul>
-					<div class="home-news-list-more">
-					<?php $page_news = get_page_by_path('espace-presse'); ?>
-					<a href="<?php echo get_permalink($page_news->ID); ?>">Espace presse</a>
-					</div>
-			</div>
-			<div class="part-title-separator">
-					<span class="part-title"> 
-						Nos partenaires
-					</span>
-				</div>
+<div id="home_middle_top">
+	<div id="home_middle_content">
+		<div class="center">
 			<?php 
-					$page_partners = get_page_by_path('partenaires');
+			$page_list_projects = get_page_by_path('les-projets');
+			$page_finance = get_page_by_path('financement');
 			?>
-			<div class="partners_zone">
-					<a href="<?php echo get_permalink($page_partners->ID); ?>"><img src="<?php echo $stylesheet_directory_uri; ?>/images/frise_partenaires_wedogood.png" width="3135" height="150" alt="logos partenaires" /></a>
-			</div>
+			<a href="<?php echo get_permalink($page_list_projects->ID); ?>" style="display: block;">
+				<div class="round_title_left">
+					<strong>Participez</strong><br/>&agrave; un projet
+				</div>
+			</a>
+			<a href="<?php echo get_permalink($page_finance->ID); ?>" style="display: block;">
+				<div class="round_title_right">
+					<strong>Financez</strong><br />un projet
+				</div>
+			</a>
+			<div class="clear"></div>
 		</div>
 	</div>
-</div><!-- #content -->
+</div>
+<div class="center">
+	<div id="home_middle_desc_left">
+		<p>
+			Soyez acteurs et influenceurs<br />
+			de la communaut&eacute;
+		</p>
+		<p>
+			<img src="<?php echo $stylesheet_directory_uri; ?>/images/main.jpg" alt="logo main" /><br />
+			Votez pour les projets les plus impactants
+		</p>
+		<p>
+			<img src="<?php echo $stylesheet_directory_uri; ?>/images/pieces.jpg" alt="logo piece monnaie" /><br />
+			Investissez &agrave; partir de 10&euro;
+		</p>
+		<p>
+			<img src="<?php echo $stylesheet_directory_uri; ?>/images/fusee.jpg" alt="logo fusee" /><br />
+			Boostez l&apos;&eacute;conomie positive
+		</p>
+	</div>
+	<div id="home_middle_desc_right">
+		<p>
+			B&eacute;n&eacute;ficiez d'un financement souple<br />
+			et adapt&eacute; &agrave; vos besoins
+		</p>
+		<p>
+			<img src="<?php echo $stylesheet_directory_uri; ?>/images/sous.jpg" alt="logo euro" /><br />
+			Trouvez un financement pour votre projet
+		</p>
+		<p>
+			<img src="<?php echo $stylesheet_directory_uri; ?>/images/hp.jpg" alt="logo megaphone" /><br />
+			Faites conna&icirc;tre votre projet
+		</p>
+		<p>
+			<img src="<?php echo $stylesheet_directory_uri; ?>/images/communaute.jpg" alt="logo communaute" /><br />
+			F&eacute;d&eacute;rez une communaut&eacute; sur la dur&eacute;e
+		</p>
+	</div>
+</div>
 
-<?php
-function wdg_showblogitem() {
-    ?>
-    <li>
-	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	<div class="description-separator "></div>
-
-	<div class="blogimg">
-	    <a href="<?php the_permalink(); ?>">
-		<?php the_post_thumbnail('thumbnail'); ?>
-	    </a>
+<div id="home_bottom" class="center">
+	<div class="padder">
+		<div class="part-title-separator">
+			<span class="part-title">Nos partenaires</span>
+		</div>
+		<?php $page_partners = get_page_by_path('partenaires'); ?>
+		<div class="partners_zone">
+			<a href="<?php echo get_permalink($page_partners->ID); ?>"><img src="<?php echo $stylesheet_directory_uri; ?>/images/frise_partenaires_wedogood.png" width="3135" height="150" alt="logos partenaires" /></a>
+		</div>
 	</div>
-	
-	<div class="blogexcerpt">
-	    <a href="<?php the_permalink(); ?>"><?php the_excerpt( ); ?></a>
-	</div>
-	
-	<div class="clear"></div>
-    </li>
-    <?php
-}
-
-function wdg_shownewsitem(){
-    ?>
-    <li>
-	<div class="news-img">
-	    <a href="<?php the_permalink(); ?>">
-		<?php the_post_thumbnail("medium"); ?>
-	    </a>
-	</div>
-	<div class="news-title">
-	    <a href="<?php the_permalink(); ?>">
-		<?php the_title(); ?>
-		
-	    </a>
-	</div>
-    </li>
-    <?php
-}
-?>
+</div>
