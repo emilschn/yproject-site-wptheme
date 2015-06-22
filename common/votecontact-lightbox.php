ca8e67bf5b67176d2facf2f05ea0d96ca821403e
TheoDel <theo.deldel@gmail.com>
2015-06-22 15:59:54 +0200

    Lightbox liste des votants

---
+++ common/votecontact-lightbox.php
@@ -0,0 +1,63 @@
+<h1>Liste des votants</h1>
+<?php if (isset($_GET['campaign_id'])) {
+    global $wpdb;
+    $table_name = $wpdb->prefix . "ypcf_project_votes";
+    $campaign_id = $_GET['campaign_id'];
+    
+    $list_user_voters = $wpdb->get_results( "SELECT user_id, invest_sum FROM ".$table_name." WHERE post_id = ".$campaign_id." AND validate_project = 1" );
+    
+    $colonnes = array('Utilisateur', 'Nom', 'Prénom', 'Ville', 'Mail', 'Montant promis');
+?>
+<em>Seuls les personnes ayant voté "Oui" sont affich&eacute;es.</em><br/><br/>
+
+<div class="tablescroll" >
+<table class="wp-list-table" cellspacing="0">
+    
+    <thead style="background-color: #CCC;">
+    <tr>
+        <?php foreach($colonnes as $titre){
+            //Ecriture des nom des colonnes en haut
+            echo '<td>'.$titre.'</td>';}?>
+    </tr>
+    </thead>
+
+    <tbody>
+	<?php
+	$i = -1;
+	foreach ( $list_user_voters as $item ) {
+            $i++;
+            $bgcolor = ($i % 2 == 0) ? "#FFF" : "#EEE";
+            $user_data = get_userdata($item->user_id);?>
+
+            <tr style="background-color: <?php echo $bgcolor; ?>">
+            <?php
+                $colonnesres = array(
+                    bp_core_get_userlink($item->user_id),
+                    $user_data->last_name,
+                    $user_data->first_name,
+                    $user_data->user_city,
+                    $user_data->user_email,
+                    $item->invest_sum
+                );
+
+                //Ecriture de la ligne
+                foreach($colonnesres as $item){
+                    echo '<td>'.$item.'</td>';
+                }
+            ?>
+            </tr>
+            <?php
+	}
+	?>
+    </tbody>
+    
+    <tfoot style="background-color: #CCC;">
+    <tr>
+        <?php foreach($colonnes as $titre){
+            //Ecriture des nom des colonnes en haut
+            echo '<td>'.$titre.'</td>';}?>
+    </tr>
+    </tfoot>
+</table>
+</div>
+<?php } ?>
--- projects/single-project-investors.php
+++ projects/single-project-investors.php
@@ -5,8 +5,8 @@ $campaign = atcf_get_current_campaign();
 if ($campaign->current_user_can_edit()) {
 ?>
 		
-<h2>Liste des investisseurs</h2>
-<i>Si vous envoyez un mail group&eacute; &agrave; vos investisseurs, pensez &agrave; les mettre dans le champ CCI, pour qu&apos;ils n&apos;aient pas acc&egrave;s aux adresses des autres.</i><br /><br />
+<h1>Liste des investisseurs</h1>
+<em>Si vous envoyez un mail group&eacute; &agrave; vos investisseurs, pensez &agrave; les mettre dans le champ CCI, pour qu&apos;ils n&apos;aient pas acc&egrave;s aux adresses des autres.</em><br /><br />
 
 <div id="ajax-investors-load" class="ajax-investments-load" style="text-align: center;" data-value="<?php echo $campaign->ID?>"><img id="ajax-loader-img" src="<?php echo get_stylesheet_directory_uri() ?>/images/loading.gif" alt="chargement" /></div>
 
