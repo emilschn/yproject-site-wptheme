dc22ba977cd706e4014850210a1266b8ee034c73
TheoDel <theo.deldel@gmail.com>
2015-07-02 10:44:22 +0200

    test d'investissements validés ou non

--- requests/investments.php
+++ requests/investments.php
@@ -27,11 +27,10 @@ function wdg_get_project_investments($camp_id, $include_pending = FALSE) {
 		'investors_string' => ''
 	);
 	foreach ( $payments_data as $item ) {
-            //Ne prend pas en compte les virements
-		if (($item['status'] == 'publish' || ($include_pending && $item['status'] == 'pending')) /*&& (isset($item['mangopay_contribution']->IsSucceeded) && $item['mangopay_contribution']->IsCompleted && $item['signsquid_status'] == 'Agreed')*/) {
+                //Prend en compte ou pas dans les stats les paiements non validés
+		if ($item['status'] == 'publish' || ($include_pending && $item['status'] == 'pending')) {
 			
                     $invest_user = get_user_by('id', $item['user']);
-                        $buffer['count_validate_investments']++;
 			if (!isset($buffer['investors_list'][$item['user']])) {
 				$buffer['count_validate_investors']++;
 				$buffer['investors_list'][$item['user']] = $item['user'];
@@ -46,7 +45,9 @@ function wdg_get_project_investments($camp_id, $include_pending = FALSE) {
 			$buffer['count_invest'] += $item['amount'];
 			$buffer['amounts_array'][] = $item['amount'];
 		}
-                if ($item['status'] == 'pending') {
+                if ($item['status'] == 'publish') {
+                    $buffer['count_validate_investments']++;
+                } else if ($item['status'] == 'pending') {
                     $buffer['count_not_validate_investments']++;
                 }
 	}
