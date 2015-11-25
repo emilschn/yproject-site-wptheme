ae588a9654da6c3910f2fff33e96967a6907cce1
Emilien Schneider <emilien.schneider@gmail.com>
2015-11-25 17:23:01 +0100

    Page projet V3 : intégration partie Résumé Signed-off-by: Emilien Schneider <emilien.schneider@gmail.com>

--- projects/single/pitch.php
+++ projects/single/pitch.php
@@ -1,32 +1,30 @@
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
+<?php
+global $campaign;
+$video_element = '';
+$img_src = '';
+//Si aucune vidéo n'est définie, on affiche l'image
+if ($campaign->video() == '') {
+	$img_src = $campaign->get_home_picture_src();
+
+//Sinon on utilise l'objet vidéo fourni par wordpress
+} else {
+	$video_element = wp_oembed_get($campaign->video(), array('width' => 580, 'height' => 325));
+}
+?>
+<div class="project-pitch center">
+	<div class="project-pitch-title">
+		<span> 
+			<?php _e('R&eacute;sum&eacute;', 'yproject'); ?>
+		</span>
+	</div>
+    
+	<div class="clearfix">
+		<div class="project-pitch-text">
+			<?php echo html_entity_decode($campaign->summary()); ?>
+		</div>
+
+		<div class="project-pitch-video" <?php if ($img_src != '') { ?>style="background-image: url('<?php echo $img_src; ?>');"<?php } ?>>
+			<?php echo $video_element; ?>
+		</div>
+	</div>
+</div>
