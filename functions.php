c0e3496c31c10033ff80f3d446abe50d80397f0c
Emilien Schneider <emilien.schneider@gmail.com>
2015-12-21 15:55:53 +0100

    Page projet et paramètres : corrections affichages et textes Signed-off-by: Emilien Schneider <emilien.schneider@gmail.com>

--- functions.php
+++ functions.php
@@ -163,10 +163,79 @@ function yproject_change_user_cap() {
 }
 add_action('init', 'yproject_change_user_cap');
 
+
+//***********************
+// Modification TINYMCE
+//***********************
+/**
+ * Ajout couleur WDG
+ */
+function yproject_mce4_options($init) {
+	$default_colours = '"000000", "Black",
+						"993300", "Burnt orange",
+						"333300", "Dark olive",
+						"003300", "Dark green",
+						"003366", "Dark azure",
+						"000080", "Navy Blue",
+						"333399", "Indigo",
+						"333333", "Very dark gray",
+						"800000", "Maroon",
+						"FF6600", "Orange",
+						"808000", "Olive",
+						"008000", "Green",
+						"008080", "Teal",
+						"0000FF", "Blue",
+						"666699", "Grayish blue",
+						"808080", "Gray",
+						"FF0000", "Red",
+						"FF9900", "Amber",
+						"99CC00", "Yellow green",
+						"339966", "Sea green",
+						"33CCCC", "Turquoise",
+						"3366FF", "Royal blue",
+						"800080", "Purple",
+						"999999", "Medium gray",
+						"FF00FF", "Magenta",
+						"FFCC00", "Gold",
+						"FFFF00", "Yellow",
+						"00FF00", "Lime",
+						"00FFFF", "Aqua",
+						"00CCFF", "Sky blue",
+						"993366", "Red violet",
+						"FFFFFF", "White",
+						"FF99CC", "Pink",
+						"FFCC99", "Peach",
+						"FFFF99", "Light yellow",
+						"CCFFCC", "Pale green",
+						"CCFFFF", "Pale cyan",
+						"99CCFF", "Light sky blue",
+						"CC99FF", "Plum"';
+
+	$custom_colours =  '"FF494C", "WE DO GOOD"';
+
+	// build colour grid default+custom colors
+	$init['textcolor_map'] = '['.$default_colours.','.$custom_colours.']';
+
+	// enable 6th row for custom colours in grid
+	$init['textcolor_rows'] = 6;
+   
+	return $init;
+}
+add_filter('tiny_mce_before_init', 'yproject_mce4_options');
+
+function yproject_additional_button($buttons) {
+   array_unshift($buttons, 'fontsizeselect');
+   return $buttons;
+}
+add_filter('mce_buttons_2', 'yproject_additional_button');
+//***********************
+// FIN - Modification TINYMCE
+//***********************
+
+
 //***********************
 // LIGHTBOX AVERTISSEMENT
 //***********************
-
 // Vérification de l'enregistrement des avertissements pour afficher la lightbox
 function yproject_check_user_warning($user_id){
     
