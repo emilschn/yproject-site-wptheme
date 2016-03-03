a12c1a739af855cd25bc26d6d774b4920ed7beee
Emilien Schneider <emilien.schneider@gmail.com>
2016-03-03 09:42:46 +0100

    Edition de la partie donnees financieres

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
 		<!--[if lt IE 9]>
 		    <script type="text/javascript" src="<?php echo $stylesheet_directory_uri; ?>/_inc/js/html5shiv.js"></script>
 		<![endif]-->
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?d=20150211" type="text/css" media="screen" />
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?d=20150303" type="text/css" media="screen" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<?php
 			$cache_head = ob_get_contents();
