bd488713ecd8f85ead968aee223a332afc1b6422
emilschn <emilien.schneider@poucr.net>
2017-01-19 10:26:54 +0100

    Integration de Crisp sur tout le site

--- footer.php
+++ footer.php
@@ -117,5 +117,7 @@ else {
 
 	<?php wp_footer(); ?>
 
+	<script type="text/javascript">$crisp=[];CRISP_WEBSITE_ID="b294206e-d4da-4d31-98c1-2581ca4fe2a9";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+
 	</body>
 </html>
--- invest/breadcrumb.php
+++ invest/breadcrumb.php
@@ -10,5 +10,3 @@
 	<div class="separator">&gt;</div>
 	<div id="share" class="invest-breadcrumb-item"><span><?php _e("Partager", 'yproject'); ?></span></div>
 </div>
-
-<script type="text/javascript">$crisp=[];CRISP_WEBSITE_ID="b294206e-d4da-4d31-98c1-2581ca4fe2a9";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
