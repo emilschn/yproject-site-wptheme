ebb331e13c3d028355fde31090a2517524e70268
Marie Morlet <marie.morlet@gmail.com>
2017-02-27 14:46:35 +0100

    Positionnement et caractère obligatoire mention déclaration capacité

--- projects/dashboard/informations/lightbox-organization-new.php
+++ projects/dashboard/informations/lightbox-organization-new.php
@@ -75,7 +75,7 @@ global $campaign_id
                 <label for="org_bankownerbic"><?php _e('BIC', 'yproject'); ?></label>
                 <input type="text" name="org_bankownerbic" value="<?php echo filter_input(INPUT_POST, 'org_bankownerbic'); ?>" /> <br />
 
-                <input type="checkbox" name="org_capable" /><?php _e('Je d&eacute;clare &ecirc;tre en capacit&eacute; de repr&eacute;senter cette organisation.', 'yproject'); ?><br />
+                <input type="checkbox" name="org_capable" /><?php _e('Je d&eacute;clare &ecirc;tre en capacit&eacute; de repr&eacute;senter cette organisation', 'yproject'); ?>*<br />
 
                 <input type="hidden" name="campaign_id" value="<?php echo $campaign_id; ?>" />
                 
