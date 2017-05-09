7bbee371558ffba420034c0bdb50f761be87eb65
emilschn <emilien.schneider@poucr.net>
2017-05-09 10:36:41 +0200

    Projet : Presentation : Commentaires masqués pour utilisateurs déconnectés

--- projects/single/comments.php
+++ projects/single/comments.php
@@ -7,33 +7,36 @@ $WDGUser_current = WDGUser::current();
 ?>
 <div class="project-comments padder">
 	<h2 class="standard">/ <?php _e('Commentaires', 'yproject'); ?> /</h2>
-    
-	<?php if ( count($comment_list) > 0 ) : ?>
-		<ul>
-		<?php foreach ($comment_list as $comment): ?>
-			<li id="comment-<?php echo $comment->comment_ID; ?>">
-				<strong><?php echo $comment->comment_author. ' (' .get_comment_date('', $comment->comment_ID). ') : '; ?></strong>
-				<?php echo $comment->comment_content; ?>
-			</li>
-		<?php endforeach; ?>
-		</ul>
-	<?php else: ?>
-		<div class="align-center"><?php _e('Aucun commentaire pour l&apos;instant.', 'yproject'); ?></div>
-	<?php endif; ?>
 	
 	<?php if (!is_user_logged_in()): ?>
 		<div class="align-center">
-			<?php _e('Vous devez &ecirc;tre connect&eacute; pour poster un commentaire.', 'yproject'); ?><br /><br />
-			<a href="#register" id="register" class="wdg-button-lightbox-open button" data-lightbox="register" data-redirect="<?php echo get_permalink(); ?>"><?php _e("Inscription", 'yproject'); ?></a>
-			<a href="#connexion" id="connexion" class="wdg-button-lightbox-open button" data-lightbox="connexion" data-redirect="<?php echo get_permalink(); ?>"><?php _e("Connexion", 'yproject'); ?></a>
+			<?php _e('Vous devez &ecirc;tre connect&eacute; pour lire et poster des commentaires.', 'yproject'); ?><br /><br />
+			<a href="#register" id="register" class="wdg-button-lightbox-open button red" data-lightbox="register" data-redirect="<?php echo get_permalink(); ?>"><?php _e("Inscription", 'yproject'); ?></a>
+			<a href="#connexion" id="connexion" class="wdg-button-lightbox-open button red" data-lightbox="connexion" data-redirect="<?php echo get_permalink(); ?>"><?php _e("Connexion", 'yproject'); ?></a>
 		</div>
-	<?php elseif (!comments_open()): ?>
-		<div class="align-center"><?php _e('Les commentaires ne sont pas ouverts.', 'yproject'); ?></div>
 	<?php else: ?>
-		<?php comment_form( array(
-				"title_reply"			=> __('Poster un commentaire', 'yproject'),
-				"comment_notes_after"	=> "",
-				"logged_in_as"	=> __('Connect&eacute; en tant que ', 'yproject') . $WDGUser_current->wp_user->user_nicename
-		), $campaign->ID ); ?>
+    
+		<?php if ( count($comment_list) > 0 ) : ?>
+			<ul>
+			<?php foreach ($comment_list as $comment): ?>
+				<li id="comment-<?php echo $comment->comment_ID; ?>">
+					<strong><?php echo $comment->comment_author. ' (' .get_comment_date('', $comment->comment_ID). ') : '; ?></strong>
+					<?php echo $comment->comment_content; ?>
+				</li>
+			<?php endforeach; ?>
+			</ul>
+		<?php else: ?>
+			<div class="align-center"><?php _e('Aucun commentaire pour l&apos;instant.', 'yproject'); ?></div>
+		<?php endif; ?>
+			
+		<?php if (!comments_open()): ?>
+			<div class="align-center"><?php _e('Les commentaires ne sont pas ouverts.', 'yproject'); ?></div>
+		<?php else: ?>
+			<?php comment_form( array(
+					"title_reply"			=> __('Poster un commentaire', 'yproject'),
+					"comment_notes_after"	=> "",
+					"logged_in_as"	=> __('Connect&eacute; en tant que ', 'yproject') . $WDGUser_current->wp_user->user_nicename
+			), $campaign->ID ); ?>
+		<?php endif; ?>
 	<?php endif; ?>
 </div>
