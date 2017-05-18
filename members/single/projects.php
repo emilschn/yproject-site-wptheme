adba4b4ad6e42ccb0a535e6c694fefbc06274737
emilschn <emilien.schneider@poucr.net>
2017-05-18 16:35:01 +0200

    Compte utilisateur : ajout attestation annuelle de royalties

--- members/single/projects.php
+++ members/single/projects.php
@@ -166,5 +166,27 @@ if (is_user_logged_in() && $display_loggedin_user) :
 	</ul>
 	<?php else: ?>
 		Aucun transfert d&apos;argent.
+		<br />
 	<?php endif; ?>
+	
+
+	<h2 class="underlined"><?php _e( 'Mes attestations de transaction annuelles', 'yproject' ); ?></h2>
+	<?php
+	$has_declaration = false;
+	$date_now = new DateTime();
+	?>
+	<?php for( $year = 2016; $year < $date_now->format('Y'); $year++ ): ?>
+		<?php if ( $WDGUser_displayed->has_royalties_for_year( $year ) ): ?>
+			<?php
+			$has_declaration = true;
+			$declaration_url = $WDGUser_displayed->get_royalties_certificate_per_year( $year );
+			?>
+			<a href="<?php echo $declaration_url; ?>" download="attestation-royalties-<?php echo $year; ?>.pdf" class="button red">Télécharger l'attestation <?php echo $year; ?></a><br /><br />
+		<?php endif; ?>
+	<?php endfor; ?>
+	<?php if ( !$has_declaration ): ?>
+		<?php _e( "Aucune", 'yproject' ); ?>
+	<?php endif; ?>
+	<br /><br /><br />
+	
 <?php endif; ?>
