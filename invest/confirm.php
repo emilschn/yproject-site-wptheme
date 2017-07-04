f28e3391720a4ea57099d85ed636faa7c6601a1d
emilschn <emilien.schneider@poucr.net>
2017-07-04 14:18:56 +0200

    Investissement : page de confirmation : correction sur formulaire de modification d'informations

--- invest/confirm.php
+++ invest/confirm.php
@@ -139,6 +139,12 @@ if (isset($campaign)):
 						<button class="button"><?php _e("Annuler mon investissement"); ?></button>
 					</form>
 					<br /><br />
+					
+					<form id="form-change-info" action="<?php echo admin_url( 'admin-post.php?action=cancel_token_investment'); ?>" method="post" style="display: none;">
+						<input type="hidden" name="campaign_id" value="<?php echo $campaign->ID; ?>" />
+						<input type="hidden" name="reason" value="change-info" />
+						<button class="button"><?php _e("Modifier ces informations"); ?></button>
+					</form>
 					<?php endif; ?>
 					
 					<?php
@@ -214,12 +220,7 @@ if (isset($campaign)):
 					<?php if ( !$wdginvestment->has_token() ): ?>
 					<a href="<?php echo home_url('/modifier-mon-compte'); ?>"><?php _e("Modifier ces informations", 'yproject'); ?></a><br /><br />
 					<?php else: ?>
-					<form action="<?php echo admin_url( 'admin-post.php?action=cancel_token_investment'); ?>" method="post" style="display: inline;">
-						<input type="hidden" name="campaign_id" value="<?php echo $campaign->ID; ?>" />
-						<input type="hidden" name="reason" value="change-info" />
-						<button class="button"><?php _e("Modifier ces informations"); ?></button>
-					</form>
-					<br /><br />
+					<a href="#" onclick="$('#form-change-info').submit();"><?php _e("Modifier ces informations", 'yproject'); ?></a><br /><br />
 					<?php endif; ?>
 
 					<?php $information_confirmed = (isset($_POST["information_confirmed"]) && $_POST["information_confirmed"] == "1") ? 'checked="checked" ' : ''; ?>
