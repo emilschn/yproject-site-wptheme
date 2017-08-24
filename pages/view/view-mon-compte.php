8cf58612ca1dc5b7dd4c6a74c40ae26f4d28b4ff
emilschn <emilien.schneider@poucr.net>
2017-08-24 10:09:17 +0200

    Page mon compte : RCF : ajout d'un bouton de déconnexion

--- pages/view/view-mon-compte.php
+++ pages/view/view-mon-compte.php
@@ -1,3 +1,9 @@
+<?php if ( ATCF_CrowdFunding::get_platform_context() != 'wedogood' ): ?>
+<div class="align-center" style="padding: 15px 0px; background: #FFFFFF;">
+	<a href="<?php echo wp_logout_url(); ?>" class="button"><?php _e( "D&eacute;connexion", 'yproject' ); ?></a>
+</div>
+<?php endif; ?>
+
 <div id="content">
 	<?php locate_template( array( 'pages/view/mon-compte/nav.php'  ), true ); ?>	
 	<?php locate_template( array( 'pages/view/mon-compte/content.php'  ), true ); ?>
