599bcfd46f43cf2b6922843d12503011d3294968
emilschn <emilien.schneider@poucr.net>
2017-09-08 16:54:02 +0200

    Page projet : vote : affichage fenetre de partage à la fin du processus

---
+++ projects/single/voteform-lightbox-share.php
@@ -0,0 +1,16 @@
+<?php global $stylesheet_directory_uri, $post; ?>
+
+<?php ob_start(); ?>
+<i><?php _e( "Vos informations ont bien &eacute;t&eacute; enregistr&eacute;es.", 'yproject' ); ?></i>
+<br /><br />
+<?php _e( "Merci d'avoir vot&eacute; sur ce projet, pensez &agrave; en parler autour de vous !", 'yproject' ); ?>
+<br /><br />
+<p class="align-center">
+	<?php locate_template( array( "projects/common/share-buttons.php" ), true, false );  ?>
+</p>
+
+<?php
+$lightbox_content = ob_get_contents();
+ob_end_clean();
+$campaign_title = $post->post_title;
+echo do_shortcode('[yproject_lightbox_cornered id="vote-share" title="'.__( "Vote sur ", 'yproject' ).$campaign_title.'"]' . $lightbox_content . '[/yproject_lightbox_cornered]');
