7aac3f1c428711373c733d544369fe936aac2dc1
emilschn <emilien.schneider@poucr.net>
2017-10-17 15:09:46 +0200

    Création template vide

---
+++ footer-empty.php
@@ -0,0 +1,10 @@
+		</div> <!-- #container -->
+
+	<?php wp_footer(); ?>
+
+	<?php if (!WP_IS_DEV_SITE): ?>
+	<script type="text/javascript">$crisp=[];CRISP_WEBSITE_ID="b294206e-d4da-4d31-98c1-2581ca4fe2a9";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+	<?php endif; ?>
+	
+	</body>
+</html>
