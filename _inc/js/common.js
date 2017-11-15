ebc869815a6eb9ed4a13ad780d43d2d6914df598
emilschn <emilien.schneider@poucr.net>
2017-11-15 15:28:59 +0100

    Analytics : suppression plugin et ajout pageview lors du clic sur une lightbox

--- _inc/js/common.js
+++ _inc/js/common.js
@@ -770,6 +770,8 @@ var WDGLightboxFunctions = (function($) {
 		},
 		
 		displaySingle: function( sLightboxId ) {
+			ga('set', { page: document.location.pathname + '#' + sLightboxId, title: document.title + ' | ' + sLightboxId });
+			ga('send', 'pageview');
 			$( ".wdg-lightbox" ).hide();
 			$( "#wdg-lightbox-" + sLightboxId ).show();
 			if( $( "#wdg-lightbox-" + sLightboxId ).data( "scrolltop" ) == "1" ){
