5eba0933dc1d91e960b004b434b4b73c30207030
emilschn <emilien.schneider@poucr.net>
2018-01-09 16:44:28 +0100

    Accueil : Reclassement des 3 campagnes

--- pages/controler/controler-home.php
+++ pages/controler/controler-home.php
@@ -9,7 +9,7 @@ class WDG_Page_Controler_Home extends WDG_Page_Controler {
 	private static $nb_query_campaign_funded = 40;
 	private static $projects_html_key = 'home-projects';
 	private static $projects_html_duration = 180; // 3 minutes de cache
-	private static $projects_html_version = 2;
+	private static $projects_html_version = 3;
 	private static $projects_nb_to_show = 3;
 	private $projects_html;
 	private $projects_list;
@@ -49,11 +49,60 @@ class WDG_Page_Controler_Home extends WDG_Page_Controler {
 /******************************************************************************/
 // PROJECT LIST
 /******************************************************************************/
+	private static $funded_campaign_top_list = array( 'naoden', 'twiza', 'blocinbloc' );
 	private function init_projects() {
 		$this->projects_html = $this->get_db_cached_elements( WDG_Page_Controler_Home::$projects_html_key, WDG_Page_Controler_Home::$projects_html_version );
 		if ( empty( $this->projects_html ) ) {
-			$this->projects_list = ATCF_Campaign::get_list_most_recent( WDG_Page_Controler_Home::$projects_nb_to_show );
+			$this->projects_list = array();
+			$campaignlist_funding = ATCF_Campaign::get_list_funding( WDG_Page_Controler_Home::$projects_nb_to_show );
+			$campaignlist_funding_sorted = $this->sort_project_list( $campaignlist_funding );
+			$count_campaignlist = count( $campaignlist_funding_sorted );
+			foreach ( $campaignlist_funding_sorted as $campaign ) { array_push( $this->projects_list, $campaign->ID ); }
+			
+			if ( $count_campaignlist < WDG_Page_Controler_Home::$projects_nb_to_show ) {
+				$campaignlist_vote = ATCF_Campaign::get_list_vote( WDG_Page_Controler_Home::$projects_nb_to_show - $count_campaignlist, '' );
+				$campaignlist_vote_sorted = $this->sort_project_list( $campaignlist_vote );
+				$count_campaignlist += count( $campaignlist_vote_sorted );
+				foreach ( $campaignlist_vote_sorted as $campaign ) { array_push( $this->projects_list, $campaign->ID ); }
+			}
+
+			if ( $count_campaignlist < WDG_Page_Controler_Home::$projects_nb_to_show ) {
+				for ( $i = 0; $i < WDG_Page_Controler_Home::$projects_nb_to_show - $count_campaignlist; $i++ ) {
+					global $wpdb;
+					$result = $wpdb->get_var( 
+						"SELECT ID FROM ".$wpdb->posts." WHERE ".$wpdb->posts.".post_type = 'download' AND ".$wpdb->posts.".post_name = '".WDG_Page_Controler_Home::$funded_campaign_top_list[ $i ]."'",
+						OBJECT
+					);
+					array_push( $this->projects_list, $result );
+				}
+			}
 		}
+	}
+	
+	private function sort_project_list( $campaign_list ) {
+		// On commence par mélanger toute la liste pour être sûr d'avoir de l'aléatoire
+		shuffle( $campaign_list );
+		
+		// On parcourt tous les projets
+		$count_campaigns = count( $campaign_list );
+		for ( $i = $count_campaigns - 1; $i >= 0; $i-- ) {
+			$campaign_post = $campaign_list[ $i ];
+			$campaign = new ATCF_Campaign( $campaign_post->ID );
+			$campaign_categories_str = $campaign->get_categories_str();
+			
+			// On supprime ceux qui ne sont pas des projets d'entreprise
+			if ( strpos( $campaign_categories_str, 'entreprises' ) === FALSE ) {
+				array_splice( $campaign_list, $i, 1 );
+			}
+			// On met au début ceux qui sont "positifs"
+			if ( strpos( $campaign_categories_str, 'environnemental' ) !== FALSE || strpos( $campaign_categories_str, 'social' ) !== FALSE ) {
+				$campaign_element = $campaign_list[ $i ];
+				array_splice( $campaign_list, $i, 1 );
+				array_unshift( $campaign_list, $campaign_element );
+			}
+		}
+		
+		return $campaign_list;
 	}
 	
 	public function get_projects_html() {
