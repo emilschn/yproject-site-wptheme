902a0610b4cc2f6f7792c5106806b8c21e4ca5c5
emilschn <emilien.schneider@poucr.net>
2018-01-11 16:18:26 +0100

    Tunnel de paiement : suppression des references à Signsquid

--- pages/view/paiement-effectue/wire-pending.php
+++ pages/view/paiement-effectue/wire-pending.php
@@ -8,24 +8,15 @@ $page_controler = WDG_Templates_Engine::instance()->get_controler();
 
 <?php _e( "Dans l'attente de votre virement, vous recevrez un e-mail rappelant les informations &agrave; nous fournir.", 'yproject' ); ?><br><br>
 
+<?php _e( "Une fois valid&eacute;, vous recevrez un e-mail confirmant votre paiement. Votre contrat d'investissement sera joint &agrave; cet e-mail.", 'yproject' ); ?><br><br>
 <?php if ( $page_controler->get_current_investment()->get_session_amount() > WDGInvestmentContract::$signature_minimum_amount ): ?>
-	<?php _e( "Une fois valid&eacute;, vous recevrez deux e-mails :", 'yproject' ); ?><br><br>
-	
-	<?php if ( ATCF_CrowdFunding::get_platform_context() == "wedogood" ): ?>
-	- <?php _e( "un e-mail envoy&eacute; par WEDOGOOD pour la confirmation de votre paiement. Cet e-mail contient votre code pour signer le pouvoir", 'yproject' ); ?><br><br>
-	<?php else: ?>
-	- <?php _e( "un e-mail envoy&eacute; pour la confirmation de votre paiement. Cet e-mail contient votre code pour signer le pouvoir", 'yproject' ); ?><br><br>
-	<?php endif; ?>
-	
-	- <?php _e( "un e-mail envoy&eacute; par notre partenaire Signsquid. Cet e-mail contient un lien vous permettant de signer le pouvoir pour le contrat d'investissement", 'yproject' ); ?><br><br>
-
-<?php else: ?>
-	<?php _e( "Une fois valid&eacute;, vous recevrez un e-mail confirmant votre paiement. Votre contrat d'investissement sera joint &agrave; cet e-mail.", 'yproject' ); ?><br><br>
-
+	<?php _e( "Sur la page suivante, un cadre sp&eacute;cifique vous invitera &agrave; signer votre contrat.", 'yproject'); ?><br>
 <?php endif; ?>
-
-<?php if ( !$page_controler->get_current_investment()->has_token() ): ?>
-	<?php _e( "Merci de vous rendre sur la page", 'yproject' ); ?> <a href="<?php echo home_url( '/mon-compte' ); ?>"><?php _e( "Mon compte", 'yproject' ); ?></a> <?php _e( "pour suivre l&apos;&eacute;volution de votre paiement.", 'yproject' ); ?><br><br>
+	
+<?php if ( $page_controler->is_preinvestment() ): ?>
+	<?php _e( "Nous vous rappelons que les conditions que vous avez accept&eacute;es sont susceptibles d'&ecirc;tre modifi&eacutes;es &agrave; l'issue de la phase de vote.", 'yproject' ); ?><br>
+	<?php _e( "Si aucun changement ne survient, votre investissement sera valid&eacute; automatiquement.", 'yproject' ); ?><br>
+	<?php _e( "Si un changement devait survenir, vous devrez confirmer ou infirmer votre investissement.", 'yproject' ); ?><br><br>
 <?php endif; ?>
 
 <div class="db-form full v3">
