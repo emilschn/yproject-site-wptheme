a279e2e2228bcacfa1580ab632942db4ce823851
emilschn <emilien.schneider@poucr.net>
2018-01-16 10:57:08 +0100

    Page projet : possibilité de preinvestir si vote déjà fait

---
+++ projects/single/preinvestment-warning-lightbox.php
@@ -0,0 +1,33 @@
+<?php global $stylesheet_directory_uri, $post; ?>
+
+<?php
+// *****************************************************************************
+// Lightbox d'avertissement de pré-investissement
+// *****************************************************************************
+$edd_settings = get_option( 'edd_settings' );
+?>
+
+<?php ob_start(); ?>
+<div id="preinvestment-warning-form" class="wdg-lightbox-ref">
+	
+	<form method="post" class="sidebar-login-form db-form v3 full ajax-form">
+		
+		<div class="align-left">
+			<?php echo apply_filters( 'the_content', $edd_settings[ 'preinvest_warning' ] ); ?>
+		</div>
+		
+		<div id="user-details-form-buttons">
+			
+			<button type="button" class="button redirect half right red" data-redirecturl="<?php echo home_url( '/investir' ) . '?campaign_id=' .$post->ID. '&invest_start=1'; ?>"><?php _e( "Continuer", 'yproject' ); ?></button>
+			
+		</div>
+		
+	</form>
+	
+</div>
+
+<?php
+$lightbox_content = ob_get_contents();
+ob_end_clean();
+echo do_shortcode('[yproject_lightbox_cornered id="preinvest-warning" title="'.__( "Avant de pr&eacute;-investir", 'yproject' ).'"]' . $lightbox_content . '[/yproject_lightbox_cornered]');
+// *****************************************************************************
