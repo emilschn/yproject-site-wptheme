ffe3ae8b21d96373b51301c372064a6674f85d25
emilschn <emilien.schneider@poucr.net>
2018-01-22 17:21:14 +0100

    Login : Suppression des lightbox de connexion / inscription

--- functions/assets-version.php
+++ functions/assets-version.php
@@ -1,2 +1,2 @@
 <?php
-define( 'ASSETS_VERSION', '1516290677577');
+define( 'ASSETS_VERSION', '1516637705449');
--- pages/view/view-connexion.php
+++ pages/view/view-connexion.php
@@ -6,11 +6,18 @@ $page_controler = WDG_Templates_Engine::instance()->get_controler();
 	<div class="padder_more">
 		<div class="center_small margin-height">
 			
-			<div class="errors align-center" style="padding: 20px 0px;">
-				<?php echo $page_controler->get_login_error_reason(); ?>
-			</div>
+			<?php if ( $page_controler->is_login_displayed() ): ?>
+				<div class="errors align-center" style="padding: 20px 0px;">
+					<?php echo $page_controler->get_login_error_reason(); ?>
+				</div>
+
+				<?php locate_template( 'common/connexion-lightbox.php', TRUE, FALSE ); ?>
 			
-			<?php locate_template( 'common/connexion-lightbox.php', TRUE, FALSE ); ?>
+			<?php else: ?>
+				<?php locate_template( 'common/register-lightbox.php', TRUE, FALSE ); ?>
+			
+			<?php endif; ?>
+			
 		</div>
 	</div>
 </div>
