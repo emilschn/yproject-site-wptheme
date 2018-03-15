6d8d698c734615e6f8784bc625f02f10cf2bb391
emilschn <emilien.schneider@poucr.net>
2018-03-15 11:09:27 +0100

    Accueil : suppression bannière titre bannière stats

--- functions/assets-version.php
+++ functions/assets-version.php
@@ -1,2 +1,2 @@
 <?php
-define( 'ASSETS_VERSION', '1521023519162');
+define( 'ASSETS_VERSION', '1521108499542');
--- pages/view/view-home.php
+++ pages/view/view-home.php
@@ -38,33 +38,32 @@ $stats_html = $page_controler->get_stats_html();
 				
 	<?php ob_start(); ?>
 				
-				<section class="project-stats">
-					<h2 class="standard">/ <?php _e( "Lev&eacute;es de fonds en royalties", 'yproject' ) ?> /</h2>
-					<div>
-						<div class="left">
-							<img src="<?php echo $stylesheet_directory_uri; ?>/images/template-home/picto-montgolfiere-blanc-h100.png" alt="montgolfiere" width="115" height="100">
-							<span>
-								<span><?php echo number_format( $stats_list[ 'count_amount' ], 0, '', ' ' ); ?> &euro;</span><br>
-								<?php _e( "lev&eacute;s", 'yproject' ); ?>
-							</span>
-						</div>
-						<div class="left">
-							<img src="<?php echo $stylesheet_directory_uri; ?>/images/template-home/picto-ensemble-blanc-h100.png" alt="ensemble" width="139" height="100">
-							<span>
-								<span><?php echo number_format( $stats_list[ 'count_people' ], 0, '', ' ' ); ?></span><br>
-								<?php _e( "investisseurs", 'yproject' ); ?>
-							</span>
-						</div>
-						<div class="left">
-							<img src="<?php echo $stylesheet_directory_uri; ?>/images/template-home/picto-monnaie-blanc-h100.png" alt="monnaie" width="102" height="100">
-							<span>
-								<span><?php echo number_format( $stats_list[ 'count_roi' ], 0, '', ' ' ); ?> &euro;</span><br>
-								<?php _e( "de royalties vers&eacute;s", 'yproject' ); ?>
-							</span>
-						</div>
-						<div class="clear"></div>
-					</div>
-				</section>
+		<section class="project-stats">
+			<div>
+				<div class="left">
+					<img src="<?php echo $stylesheet_directory_uri; ?>/images/template-home/picto-montgolfiere-blanc-h100.png" alt="montgolfiere" width="115" height="100">
+					<span>
+						<span><?php echo number_format( $stats_list[ 'count_amount' ], 0, '', ' ' ); ?> &euro;</span><br>
+						<?php _e( "lev&eacute;s", 'yproject' ); ?>
+					</span>
+				</div>
+				<div class="left">
+					<img src="<?php echo $stylesheet_directory_uri; ?>/images/template-home/picto-ensemble-blanc-h100.png" alt="ensemble" width="139" height="100">
+					<span>
+						<span><?php echo number_format( $stats_list[ 'count_people' ], 0, '', ' ' ); ?></span><br>
+						<?php _e( "investisseurs", 'yproject' ); ?>
+					</span>
+				</div>
+				<div class="left">
+					<img src="<?php echo $stylesheet_directory_uri; ?>/images/template-home/picto-monnaie-blanc-h100.png" alt="monnaie" width="102" height="100">
+					<span>
+						<span><?php echo number_format( $stats_list[ 'count_roi' ], 0, '', ' ' ); ?> &euro;</span><br>
+						<?php _e( "de royalties vers&eacute;s", 'yproject' ); ?>
+					</span>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</section>
 				
 	<?php
 	$cache_stats = ob_get_contents();
