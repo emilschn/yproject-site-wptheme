4be1084ec4372c557aa5d930980c58e9cdf39b4b
emilschn <emilien.schneider@poucr.net>
2018-04-17 16:23:55 +0200

    Regen assets-version

--- functions/assets-version.php
+++ functions/assets-version.php
@@ -1,6 +1,2 @@
 <?php
-<<<<<<< HEAD
-define( 'ASSETS_VERSION', '1522156437861');
-=======
-define( 'ASSETS_VERSION', '1523546178712');
->>>>>>> investments-to-api
+define( 'ASSETS_VERSION', '1523975022951');
