a39564a549dc3a962e36fa8c7689432c6f0f477a
emilschn <emilien.schneider@poucr.net>
2018-06-15 16:25:50 +0200

    Mon compte : ajout infos sur LW dans les parties d'authentification

--- pages/view/mon-compte/content-orga-authentication.php
+++ pages/view/mon-compte/content-orga-authentication.php
@@ -5,3 +5,7 @@ global $WDGOrganization;
 <h2 class="underlined"><?php _e( 'Authentification de ', 'yproject' ); echo $WDGOrganization->get_name(); ?></h2>
 
 <?php locate_template( array( 'pages/view/mon-compte/partial-authentication.php' ), true, false ); ?>
+
+<br><br>
+<?php $edd_settings = get_option( 'edd_settings' ); ?>
+<?php echo wpautop( $edd_settings[ 'lemonway_generalities' ] );
