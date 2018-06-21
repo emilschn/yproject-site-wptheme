e08a08e24d8ca3945087347943e701defe18f212
emilschn <emilien.schneider@poucr.net>
2018-06-21 10:03:38 +0200

    Page projet : corrections affichage et liens

--- functions.php
+++ functions.php
@@ -529,6 +529,12 @@ function yproject_save_edit_project() {
 		'projects-current',
 		'projects-others'
 	));
+	
+	$campaign = new ATCF_Campaign( $_POST['id_campaign'] );
+	if ( $campaign->campaign_status() == ATCF_Campaign::$campaign_status_vote || $campaign->campaign_status() == ATCF_Campaign::$campaign_status_collecte || $campaign->campaign_status() == ATCF_Campaign::$campaign_status_funded ) {
+		$file_cacher = WDG_File_Cacher::current();
+		$file_cacher->build_campaign_page_cache( $this->campaign->ID );
+	}
 	echo $_POST['property'];
 	exit();
 }
--- functions/assets-version.php
+++ functions/assets-version.php
@@ -1,2 +1,2 @@
 <?php
-define( 'ASSETS_VERSION', '1529505073748');
+define( 'ASSETS_VERSION', '1529567272000');
