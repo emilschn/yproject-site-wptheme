36c554167e2ec78c17031dbd830e333117d71694
emilschn <emilien.schneider@poucr.net>
2018-06-29 15:50:13 +0200

    Projet : Evaluation : ajout d'un message dans la fenetre de partage, si l'utilisateur n'est pas authentifié

--- functions/assets-version.php
+++ functions/assets-version.php
@@ -1,2 +1,2 @@
 <?php
-define( 'ASSETS_VERSION', '1530277656715');
+define( 'ASSETS_VERSION', '1530280089138');
--- header.php
+++ header.php
@@ -156,7 +156,7 @@
 					?>
 					<span id="submenu-user-hello"><?php _e("Bonjour", 'yproject'); ?> <?php echo $user_name_str; ?> !</span>
 					<ul class="submenu-list">
-						<li><a href="<?php echo home_url( '/mon-compte/' ); ?>"><?php _e("Mon compte", 'yproject'); ?></a></li>
+						<li><a href="<?php echo home_url( '/mon-compte/' ); ?>" <?php if ( $page_controler->get_show_user_needs_authentication() ): ?>class="needs-authentication"<?php endif; ?>><?php _e("Mon compte", 'yproject'); ?></a></li>
 						
 						<?php foreach ($project_list as $project_id): if (!empty($project_id)): $post_campaign = get_post($project_id); if (isset($post_campaign)): ?>
 							<li><a href="<?php echo $page_dashboard . '?campaign_id=' .$project_id; ?>"><?php echo $post_campaign->post_title; ?></a></li>
