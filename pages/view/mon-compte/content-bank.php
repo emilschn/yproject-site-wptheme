ca9c37443ce22e374bb0c4b592d0376c55e4106b
emilschn <emilien.schneider@poucr.net>
2018-07-05 15:40:26 +0200

    Mon compte : modification affichage RIB validé et ajout options sur champs fichiers

--- pages/view/mon-compte/content-bank.php
+++ pages/view/mon-compte/content-bank.php
@@ -37,7 +37,13 @@ if ( $page_controler->is_iban_validated() ): ?>
 		</div>
 	</div>
 
-	<?php print_r( $WDGUser_lw_bank_info ); ?>
+	<?php _e( "Le RIB valid&eacute; est le suivant :", 'yproject' ); ?><br>
+	<strong><?php _e( "Propri&eacute;taire du compte :" );?></strong><br>
+	<?php echo $WDGUser_lw_bank_info->HOLDER; ?><br>
+	<strong><?php _e( "IBAN :" );?></strong><br>
+	<?php echo $WDGUser_lw_bank_info->DATA; ?><br>
+	<strong><?php _e( "BIC :" );?></strong><br>
+	<?php echo $WDGUser_lw_bank_info->SWIFT; ?><br>
 
 	<?php
 	$current_filelist_bank = WDGKYCFile::get_list_by_owner_id( $page_controler->get_current_user()->get_wpref(), WDGKYCFile::$owner_user, WDGKYCFile::$type_bank );
@@ -90,7 +96,12 @@ elseif( $page_controler->is_iban_waiting() ): ?>
 		</div>
 	</div>
 
-	<?php print_r( $WDGUser_lw_bank_info ); ?>
+	<strong><?php _e( "Propri&eacute;taire du compte :" );?></strong><br>
+	<?php echo $WDGUser_lw_bank_info->HOLDER; ?><br>
+	<strong><?php _e( "IBAN :" );?></strong><br>
+	<?php echo $WDGUser_lw_bank_info->DATA; ?><br>
+	<strong><?php _e( "BIC :" );?></strong><br>
+	<?php echo $WDGUser_lw_bank_info->SWIFT; ?><br>
 
 	<?php
 	$current_filelist_bank = WDGKYCFile::get_list_by_owner_id( $page_controler->get_current_user()->get_wpref(), WDGKYCFile::$owner_user, WDGKYCFile::$type_bank );
