233f5acc8425a8bffd99f9ddb91891cb89061dbf
emilschn <emilien.schneider@poucr.net>
2018-08-30 11:38:01 +0200

    TBPP : Onglet guide et outils : modification texte par défaut et possibilité de surcharger avec une page spécifique via WP

--- pages/view/tableau-de-bord/tab-tools.php
+++ pages/view/tableau-de-bord/tab-tools.php
@@ -2,14 +2,29 @@
 $page_controler = WDG_Templates_Engine::instance()->get_controler();
 ?>
 
+<?php
+$post_guide_tool_content_filtered = FALSE;
+$post_guide_tool = get_page_by_path( 'tableau-de-bord-onglet-outil' );
+if ( $post_guide_tool ) {
+	$post_guide_tool_content = $post_guide_tool->post_content;
+	$post_guide_tool_content_filtered = wpautop( $post_guide_tool_content );
+}
+?>
+
 <h2><?php _e( "Guide et outils", 'yproject' ); ?></h2>
 <div class="db-form v3 center">
-	<br>
-	<p class="align-justify">
-		<?php _e( "Le guide de campagne vous met &agrave; disposition des conseils pratiques sur tous les aspects de votre communication pour votre campagne.", 'yproject' ); ?><br>
-		<?php _e( "Nous sommes aussi disponibles via le chat en ligne ou &agrave; l'adresse suivante : support@wedogood.co.", 'yproject' ); ?><br>
-	</p>
-	<br><br>
+	<?php if ( !empty( $post_guide_tool_content_filtered ) ): ?>
+		<?php echo $post_guide_tool_content_filtered; ?>
 	
-	<a href="<?php echo home_url( '/guide/' ); ?>" class="button red" target="_blank"><?php _e( "Consulter le guide", 'yproject' ); ?></a>
+	<?php else: ?>
+		<br>
+		<h3><?php _e( "Centre de support", 'yproject' ); ?></h3>
+		<p class="align-justify">
+			<?php _e( "Le centre de support regroupe toutes les FAQ et met &agrave; votre disposition des conseils pratiques sur la pr&eacute;paration de votre lev&eacute;e de fonds, l'animation de votre campagne, ainsi que le versement de royalties.", 'yproject' ); ?><br>
+			<?php _e( "Nous sommes aussi disponibles via le chat en ligne ou &agrave; l'adresse suivante : support@wedogood.co.", 'yproject' ); ?><br>
+		</p>
+		<br><br>
+
+		<a href="https://support.wedogood.co/comment-animer-ma-campagne-de-financement" class="button red" target="_blank"><?php _e( "Consulter le centre de support", 'yproject' ); ?></a>
+	<?php endif; ?>
 </div>
