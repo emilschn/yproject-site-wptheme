423956bdd7ba9acefce7b23a52d0bda290d31f2c
emilschn <emilien.schneider@poucr.net>
2018-09-04 15:43:14 +0200

    TBPP : Récupération des données pour construire le nouvel onglet statistiques

--- pages/controler/controler-tableau-de-bord.php
+++ pages/controler/controler-tableau-de-bord.php
@@ -161,4 +161,157 @@ class WDG_Page_Controler_Project_Dashboard extends WDG_Page_Controler {
 			exit();
 		}
 	}
+	
+/******************************************************************************/
+// PREPARATION DES DONNEES DE STATS
+/******************************************************************************/
+	public function get_campaign_stats() {
+		$buffer = array();
+		
+		$buffer[ 'name' ] = $this->campaign->get_name();
+		$buffer[ 'goal' ] = $this->campaign->minimum_goal();
+		$buffer[ 'average_median_for_campaign' ] = 40000;
+		
+		
+		// ***************
+		// Stats des votes
+		$vote_results = WDGCampaignVotes::get_results( $this->campaign_id );
+		$buffer[ 'vote' ] = array();
+		$date_begin = new DateTime( $this->campaign->get_begin_vote_str() );
+		$buffer[ 'vote' ][ 'start' ] = $date_begin->format( 'Y-m-d\Th:i' );
+		$date_end = new DateTime( $this->campaign->get_end_vote_str() );
+		$buffer[ 'vote' ][ 'end' ] = $date_end->format( 'Y-m-d\Th:i' );
+		
+		// Objectifs
+		$buffer[ 'vote' ][ 'nb' ] = array();
+		$buffer[ 'vote' ][ 'nb' ][ 'current' ] = max( 0, $vote_results[ 'count_voters' ] );
+		$buffer[ 'vote' ][ 'nb' ][ 'min' ] = 50;
+		$buffer[ 'vote' ][ 'nb' ][ 'average' ] = 60; // TODO
+		$buffer[ 'vote' ][ 'nb' ][ 'median' ] = 55; // TODO
+		$buffer[ 'vote' ][ 'nb_intent' ] = array();
+		$buffer[ 'vote' ][ 'nb_intent' ][ 'current' ] = max( 0, $vote_results[ 'count_invest_ready' ] );
+		$buffer[ 'vote' ][ 'nb_intent' ][ 'min' ] = 25;
+		$buffer[ 'vote' ][ 'nb_intent' ][ 'average' ] = 35; // TODO
+		$buffer[ 'vote' ][ 'nb_intent' ][ 'median' ] = 30; // TODO
+		$buffer[ 'vote' ][ 'amount_intent' ] = array();
+		$buffer[ 'vote' ][ 'amount_intent' ][ 'current' ] = max( 0, $vote_results[ 'sum_invest_ready' ] );
+		$buffer[ 'vote' ][ 'amount_intent' ][ 'min' ] = 40000; // TODO
+		$buffer[ 'vote' ][ 'amount_intent' ][ 'average' ] = 55000; // TODO
+		$buffer[ 'vote' ][ 'amount_intent' ][ 'median' ] = 50000; // TODO
+		$buffer[ 'vote' ][ 'nb_preinvestment' ] = array();
+		$buffer[ 'vote' ][ 'nb_preinvestment' ][ 'current' ] = max( 0, $vote_results[ 'count_preinvestments' ] );
+		$buffer[ 'vote' ][ 'nb_preinvestment' ][ 'min' ] = 15; // TODO
+		$buffer[ 'vote' ][ 'nb_preinvestment' ][ 'average' ] = 25; // TODO
+		$buffer[ 'vote' ][ 'nb_preinvestment' ][ 'median' ] = 20; // TODO
+		$buffer[ 'vote' ][ 'amount_preinvestment' ] = array();
+		$buffer[ 'vote' ][ 'amount_preinvestment' ][ 'current' ] = max( 0, $vote_results[ 'amount_preinvestments' ] );
+		$buffer[ 'vote' ][ 'amount_preinvestment' ][ 'min' ] = 10000; // TODO
+		$buffer[ 'vote' ][ 'amount_preinvestment' ][ 'average' ] = 25000; // TODO
+		$buffer[ 'vote' ][ 'amount_preinvestment' ][ 'median' ] = 20000; // TODO
+		$buffer[ 'vote' ][ 'average_intent' ] = max( 0, $vote_results[ 'average_invest_ready' ] );
+		$buffer[ 'vote' ][ 'percent_intent' ] = max( 0, $vote_results[ 'count_invest_ready' ] ) / $vote_results[ 'count_voters' ] * 100;
+		
+		// Liste
+		$buffer[ 'vote' ][ 'list_vote' ] = array();
+		$buffer[ 'vote' ][ 'list_vote' ][ 'current' ] = array();
+		foreach ( $vote_results[ 'list_sum_by_date' ] as $vote_result ) {
+			$vote_item = array(
+				'date' => $vote_result[ 'date' ],
+				'sum' => max( 0, $vote_result[ 'sum' ] )
+			);
+			array_push( $buffer[ 'vote' ][ 'list_vote' ][ 'current' ], $vote_item );
+		}
+		$buffer[ 'vote' ][ 'list_vote' ][ 'target' ] = array(); // TODO
+		
+		// Préinvestissements
+		$buffer[ 'vote' ][ 'list_preinvestement' ] = array();
+		$buffer[ 'vote' ][ 'list_preinvestement' ][ 'current' ] = array();
+		foreach ( $vote_results[ 'list_preinvestments' ] as $preinvestment ) {
+			$preinvestment_item = array(
+				'date' => $preinvestment[ 'date' ],
+				'sum' => $preinvestment[ 'sum' ]
+			);
+			array_push( $buffer[ 'vote' ][ 'list_preinvestement' ][ 'current' ], $preinvestment_item );
+		}
+		$buffer[ 'vote' ][ 'list_preinvestement' ][ 'target' ] = array(); // TODO
+		
+		// Notes
+		$buffer[ 'vote' ][ 'rates' ] = array();
+		$buffer[ 'vote' ][ 'rates' ][ 'economy' ] = $vote_results[ 'average_impact_economy' ];
+		$buffer[ 'vote' ][ 'rates' ][ 'environment' ] = $vote_results[ 'average_impact_environment' ];
+		$buffer[ 'vote' ][ 'rates' ][ 'social' ] = $vote_results[ 'average_impact_social' ];
+		$buffer[ 'vote' ][ 'rates' ][ 'others' ] = $vote_results[ 'list_impact_others_string' ];
+		$buffer[ 'vote' ][ 'rates' ][ 'project' ] = array();
+		$buffer[ 'vote' ][ 'rates' ][ 'project' ][ '1' ] = max( 0, $vote_results[ 'rate_project_list' ][ 1 ] );
+		$buffer[ 'vote' ][ 'rates' ][ 'project' ][ '2' ] = max( 0, $vote_results[ 'rate_project_list' ][ 2 ] );
+		$buffer[ 'vote' ][ 'rates' ][ 'project' ][ '3' ] = max( 0, $vote_results[ 'rate_project_list' ][ 3 ] );
+		$buffer[ 'vote' ][ 'rates' ][ 'project' ][ '4' ] = max( 0, $vote_results[ 'rate_project_list' ][ 4 ] );
+		$buffer[ 'vote' ][ 'rates' ][ 'project' ][ '5' ] = max( 0, $vote_results[ 'rate_project_list' ][ 5 ] );
+		$buffer[ 'vote' ][ 'rates' ][ 'project' ][ 'average' ] = max( 0, $vote_results[ 'rate_project_average' ] );
+		$buffer[ 'vote' ][ 'rates' ][ 'project' ][ 'positive_percent' ] = max( 0, $vote_results[ 'percent_project_not_validated' ] );
+		
+		// Risque
+		$buffer[ 'vote' ][ 'risk' ] = array();
+		$buffer[ 'vote' ][ 'risk' ][ '1' ] = max( 0, $vote_results[ 'risk_list' ][ 1 ] );
+		$buffer[ 'vote' ][ 'risk' ][ '2' ] = max( 0, $vote_results[ 'risk_list' ][ 2 ] );
+		$buffer[ 'vote' ][ 'risk' ][ '3' ] = max( 0, $vote_results[ 'risk_list' ][ 3 ] );
+		$buffer[ 'vote' ][ 'risk' ][ '4' ] = max( 0, $vote_results[ 'risk_list' ][ 4 ] );
+		$buffer[ 'vote' ][ 'risk' ][ '5' ] = max( 0, $vote_results[ 'risk_list' ][ 5 ] );
+		$buffer[ 'vote' ][ 'risk' ][ 'average' ] = max( 0, $vote_results[ 'average_risk' ] );
+		
+		// Plus d'infos
+		$buffer[ 'vote' ][ 'more_info' ] = array();
+		$buffer[ 'vote' ][ 'more_info' ][ 'impact' ] = max( 0, $vote_results[ 'count_more_info_impact' ] );
+		$buffer[ 'vote' ][ 'more_info' ][ 'product' ] = max( 0, $vote_results[ 'count_more_info_service' ] );
+		$buffer[ 'vote' ][ 'more_info' ][ 'team' ] = max( 0, $vote_results[ 'count_more_info_team' ] );
+		$buffer[ 'vote' ][ 'more_info' ][ 'finance' ] = max( 0, $vote_results[ 'count_more_info_finance' ] );
+		$buffer[ 'vote' ][ 'more_info' ][ 'other' ] = max( 0, $vote_results[ 'count_more_info_other' ] );
+		$buffer[ 'vote' ][ 'more_info' ][ 'others' ] = array();
+		foreach ( $vote_results[ 'list_more_info_other' ] as $more_info_other_item ) {
+			$userdata = get_userdata( $more_info_other_item[ 'user_id' ] );
+			$buffer[ 'vote' ][ 'more_info' ][ 'others' ][ $userdata->user_email ] = $more_info_other_item[ 'text' ];
+		}
+		
+		
+		// ***************
+		// Stats des investissements
+		$investment_results = WDGCampaignInvestments::get_list( $this->campaign_id );
+		$buffer[ 'funding' ] = array();
+		$buffer[ 'funding' ][ 'start' ] = $this->campaign->begin_collecte_date( 'Y-m-d\Th:i' );
+		$buffer[ 'funding' ][ 'end' ] = $this->campaign->end_date( 'Y-m-d\Th:i' );
+		$buffer[ 'funding' ][ 'nb_investment' ] = array();
+		$buffer[ 'funding' ][ 'nb_investment' ][ 'current' ] = max( 0, $investment_results[ 'count_validate_investments' ] );
+		$buffer[ 'funding' ][ 'nb_investment' ][ 'current_different' ] = max( 0, $investment_results[ 'count_validate_investors' ] );
+		$buffer[ 'funding' ][ 'nb_investment' ][ 'not_validated' ] = max( 0, $investment_results[ 'count_not_validate_investments' ] );
+		$buffer[ 'funding' ][ 'nb_investment' ][ 'average' ] = 75;
+		$buffer[ 'funding' ][ 'nb_investment' ][ 'median' ] = 70;
+		$buffer[ 'funding' ][ 'amount_investment' ] = array();
+		$buffer[ 'funding' ][ 'amount_investment' ][ 'current' ] = $this->campaign->current_amount( FALSE );
+		$buffer[ 'funding' ][ 'amount_investment' ][ 'average' ] = 55000;
+		$buffer[ 'funding' ][ 'amount_investment' ][ 'median' ] = 50000;
+		
+		// Liste
+		$buffer[ 'funding' ][ 'list_investement' ] = array();
+		$buffer[ 'funding' ][ 'list_investement' ][ 'current' ] = array();
+		foreach ( $investment_results[ 'payments_data' ] as $investment_result ) {
+			$investment_date = new DateTime( $investment_result[ 'date' ] );
+			$investment_item = array(
+				'date' => $investment_date->format( 'Y-m-d\Th:i' ),
+				'sum' => max( 0, $investment_result[ 'amount' ] )
+			);
+			array_push( $buffer[ 'funding' ][ 'list_investement' ][ 'current' ], $investment_item );
+		}
+		$buffer[ 'funding' ][ 'list_investement' ][ 'target' ] = array();
+		
+		// Stats
+		$buffer[ 'funding' ][ 'stats' ][ 'age' ] = max( 0, $investment_results[ 'average_age' ] );
+		$buffer[ 'funding' ][ 'stats' ][ 'percent_men' ] = max( 0, $investment_results[ 'percent_male' ] );
+		$buffer[ 'funding' ][ 'stats' ][ 'percent_women' ] = max( 0, $investment_results[ 'percent_female' ] );
+		$buffer[ 'funding' ][ 'stats' ][ 'invest_average' ] = max( 0, $investment_results[ 'average_invest' ] );
+		$buffer[ 'funding' ][ 'stats' ][ 'invest_min' ] = max( 0, $investment_results[ 'min_invest' ] );
+		$buffer[ 'funding' ][ 'stats' ][ 'invest_median' ] = max( 0, $investment_results[ 'median_invest' ] );
+		$buffer[ 'funding' ][ 'stats' ][ 'invest_max' ] = max( 0, $investment_results[ 'max_invest' ] );
+		
+		return $buffer;
+	}
 }
--- pages/view/tableau-de-bord/tab-stats.php
+++ pages/view/tableau-de-bord/tab-stats.php
@@ -1,13 +1,11 @@
 <?php $page_controler = WDG_Templates_Engine::instance()->get_controler(); ?>
 
-<h2><?php _e( "Statistiques", 'yproject' ); ?></h2>
-<h2 class="expandator" data-target="votes"><?php _e( "&Eacute;valuations", 'yproject' ); ?> <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/plus.png" alt="signe plus"></h2>
-<div id="extendable-votes" class="expandable">
-    <?php locate_template( array("projects/single-stats-advanced-votes.php"), true );?>
-</div>
+<?php
+// Préparation des statistiques
+global $campaign_stats;
+$campaign_stats = $page_controler->get_campaign_stats();
+?>
 
-<?php $campaign = atcf_get_current_campaign(); ?>
-<h2 class="expandator" data-target="investments"><?php _e( "Investissements", 'yproject' ); ?> <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/plus.png" alt="signe plus"></h2>
-<div id="extendable-investments" class="expandable">
-    <?php locate_template( array("projects/single-stats-advanced-investments.php"), true );?>
-</div>
+<?php
+// Inclusion des fichiers externes dans /tab-stats/
+print_r( $campaign_stats );
