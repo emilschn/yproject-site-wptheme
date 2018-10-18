4d2652f5836404e42c6ad90e75c97d3a0f03c41c
emilschn <emilien.schneider@poucr.net>
2018-10-18 17:42:19 +0200

    Mon compte : ajout onglet Notifications

---
+++ pages/view/mon-compte/content-user-notifications.php
@@ -0,0 +1,36 @@
+<?php
+$page_controler = WDG_Templates_Engine::instance()->get_controler();
+$WDGUser_displayed = $page_controler->get_current_user();
+$WDGUserNotificationsForm = $page_controler->get_user_notifications_form();
+$fields_hidden = $WDGUserNotificationsForm->getFields( WDG_Form_User_Notifications::$field_group_hidden );
+$fields_newsletters = $WDGUserNotificationsForm->getFields( WDG_Form_User_Notifications::$field_group_newsletters );
+$fields_projects = $WDGUserNotificationsForm->getFields( WDG_Form_User_Notifications::$field_group_projects );
+?>
+
+<form method="POST" enctype="multipart/form-data" class="db-form v3 full form-register">
+	<h2><?php _e( "Notifications en provenance de WE DO GOOD", 'yproject' ); ?></h2>
+
+	<?php foreach ( $fields_hidden as $field ): ?>
+		<?php global $wdg_current_field; $wdg_current_field = $field; ?>
+		<?php locate_template( array( "common/forms/field.php" ), true, false );  ?>
+	<?php endforeach; ?>
+		
+	<h3 class="align-left uppercase"><?php _e( "Newsletters", 'yproject' ); ?></h3>
+
+	<?php foreach ( $fields_newsletters as $field ): ?>
+		<?php global $wdg_current_field; $wdg_current_field = $field; ?>
+		<?php locate_template( array( "common/forms/field.php" ), true, false );  ?>
+	<?php endforeach; ?>
+		
+	<h3 class="align-left uppercase"><?php _e( "Projets suivis", 'yproject' ); ?></h3>
+
+	<?php foreach ( $fields_projects as $field ): ?>
+		<?php global $wdg_current_field; $wdg_current_field = $field; ?>
+		<?php locate_template( array( "common/forms/field.php" ), true, false );  ?>
+	<?php endforeach; ?>
+
+	<div id="user-notifications-form-buttons">
+		<button type="submit" class="button save red"><?php _e( "Enregistrer les modifications", 'yproject' ); ?></button>
+	</div>
+	
+</form>
