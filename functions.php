5fda47fe9c49dad9694ae03c88bcb485c93978db
emilschn <emilien.schneider@poucr.net>
2019-06-28 13:02:36 +0200

    Fonctions : ajout array_key_last pour version PHP ancienne

--- functions.php
+++ functions.php
@@ -592,3 +592,12 @@ if ( !function_exists( 'array_key_first' ) ) {
 		return key( $array );
 	}
 }
+
+if ( !function_exists( 'array_key_last' ) ) {
+	function array_key_last( $array ) {
+		if ( !is_array( $array ) || empty( $array ) ) {
+			return NULL;
+		}
+		return array_keys( $array )[ count( $array ) - 1 ];
+	}
+}
