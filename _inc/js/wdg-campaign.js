b1b5149a6253bf3ee91788ffee44d2188ce7a475
hmeneuvrier <33071753+hmeneuvrier@users.noreply.github.com>
2020-03-23 11:25:38 +0100

    Asana 1162827664372623 : retours de PR

--- _inc/js/wdg-campaign.js
+++ _inc/js/wdg-campaign.js
@@ -238,7 +238,6 @@ var WDGProjectViewer = (function($) {
 			if ($("div#content.version-3 nav.project-navigation").length > 0) {
 				$("div#content.version-3 nav.project-navigation ul li a").removeClass("selected");
 				for (i = 0; i < WDGProjectViewer.nProjectParts; i++) {
-					console.log(WDGProjectViewer.aProjectParts[i]);
 					if ($(document).scrollTop() >= $("div.project-" + WDGProjectViewer.aProjectParts[i]).offset().top - $("nav#navigation").height() - $("nav.project-navigation").height()) {
 						$("div#content.version-3 nav.project-navigation ul li a#target-" + WDGProjectViewer.aProjectParts[i]).addClass("selected");
 						break;
