4fc41bd20a5b693e9460c409e79adfadab7a16eb
emilschn <emilien.schneider@poucr.net>
2020-06-16 15:55:49 +0200

    Page projet : mise en place des commentaires standards pour pouvoir répondre

--- projects/single/comments.php
+++ projects/single/comments.php
@@ -1,9 +1,5 @@
 <?php
 global $campaign;
-$comment_list = get_comments(array(
-	'post_id'	=> $campaign->ID
-));
-$WDGUser_current = WDGUser::current();
 ?>
 <div class="project-comments padder">
 	<h2 class="standard">/ <?php _e('Commentaires', 'yproject'); ?> /</h2>
@@ -16,22 +12,16 @@ $WDGUser_current = WDGUser::current();
 		</div>
 	
 	<?php else: ?>
-    
-		<?php if ( count($comment_list) > 0 ) : ?>
-			<ul>
-			<?php foreach ($comment_list as $comment): ?>
-				<?php $WDGUser_author = new WDGUser( $comment->user_id, FALSE ); ?>
-				<li id="comment-<?php echo $comment->comment_ID; ?>">
-					<strong><?php echo $WDGUser_author->get_display_name(). ' (' .get_comment_date('', $comment->comment_ID). ') : '; ?></strong>
-					<?php echo $comment->comment_content; ?>
-				</li>
-			<?php endforeach; ?>
-			</ul>
-		<?php else: ?>
-			<div class="align-center"><?php _e('Aucun commentaire pour l&apos;instant.', 'yproject'); ?></div>
-		<?php endif; ?>
-			
-			
+		
+		<?php
+		$comments = get_comments(array(
+			'post_id'	=> $campaign->ID,
+			'status'	=> 'approve'
+		));
+		?>
+
+		<?php wp_list_comments( array(), $comments ); ?>
+
 		<?php if ( !is_user_logged_in() ): ?>
 			<div class="align-center">
 				<br><br>
@@ -41,17 +31,8 @@ $WDGUser_current = WDGUser::current();
 			</div>
 			
 		<?php else: ?>
-			
-			<?php if ( !comments_open( $campaign->ID ) ): ?>
-				<div class="align-center"><?php _e('Les commentaires ne sont pas ouverts.', 'yproject'); ?></div>
-				
-			<?php else: ?>
-				<?php comment_form( array(
-						"title_reply"			=> __('Poster un commentaire', 'yproject'),
-						"comment_notes_after"	=> "",
-						"logged_in_as"	=> __('Connect&eacute;(e) en tant que ', 'yproject') . $WDGUser_current->get_display_name()
-				), $campaign->ID ); ?>
-			<?php endif; ?>
+			<?php comment_form( array(), $campaign->ID ); ?>
+
 		<?php endif; ?>
 	<?php endif; ?>
 </div>
