dc68ffcbf64a78eb5d6e3dd1d39ca5a308910fd3
emilschn <emilien.schneider@poucr.net>
2020-06-18 16:30:42 +0200

    Traduction mon compte : structure pour le menu

--- pages/view/view-mon-compte.php
+++ pages/view/view-mon-compte.php
@@ -1,22 +1,16 @@
 <?php $page_controler = WDG_Templates_Engine::instance()->get_controler(); ?>
 
-<?php if ( ATCF_CrowdFunding::get_platform_context() != 'wedogood' ): ?>
-<div class="align-center" style="padding: 15px 0px; background: #FFFFFF;">
-	<a href="<?php echo wp_logout_url(); ?>" class="button"><?php _e( "D&eacute;connexion", 'yproject' ); ?></a>
-</div>
-<?php endif; ?>
-
 <div id="content">
 
 	<?php if ( $page_controler->is_displayed_user_override_not_found() ): ?>
 	<div class="wdg-message error">
-		<?php _e( "L'utilisateur que vous essayez d'afficher n'existe pas...", 'yproject' ); ?>
+		<?php _e( 'account.OVERRIDE_USER_DOESNT_EXIST', 'yproject' ); ?>
 	</div>
 
 	<?php elseif ( $page_controler->is_displayed_user_override_organization() ): ?>
 	<div class="wdg-message error">
-		<?php _e( "L'utilisateur que vous essayez d'afficher est une organisation.", 'yproject' ); ?><br>
-		<?php _e( "L'adresse e-mail de la personne physique qui g&egrave;re ce compte est :", 'yproject' ); ?> <?php echo $page_controler->user_override_organization_manager_mail(); ?>.
+		<?php _e( 'account.OVERRIDE_USER_IS_ORGANIZATION', 'yproject' ); ?><br>
+		<?php _e( 'account.OVERRIDE_ORGANIZATION_MANAGER_EMAIL', 'yproject' ); ?> <?php echo $page_controler->user_override_organization_manager_mail(); ?>.
 	</div>
 	
 	<?php else: ?>
