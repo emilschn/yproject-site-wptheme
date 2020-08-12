503f7c71c9ceca6369372fc763dc0848766de567
emilschn <emilien.schneider@poucr.net>
2020-08-12 12:42:40 +0200

    Page projet : pouvoir afficher la présentation des projets terminés que pour les investisseurs

---
+++ projects/single/description-hidden.php
@@ -0,0 +1,22 @@
+<?php global $stylesheet_directory_uri; ?>
+<div class="project-description">
+	<h2 class="standard">/ <?php _e('Pr&eacute;sentation', 'yproject'); ?> /</h2>
+	
+	<div class="center">
+
+		<div class="project-description-item">
+			<div class="projects-desc-content-picto">
+				<img class="project-content-icon vertical-align-middle" src="<?php echo $stylesheet_directory_uri; ?>/images/template-project/description-pitch-hidden.png" alt="project" width="77" />
+			</div>
+			<div id="project-content-description" class="projects-desc-content">
+				<div class="zone-content align-center">
+					<br><br>
+					<strong><?php _e( "Vous ne pouvez pas acc&eacute;der &agrave; la pr&eacute;sentation et aux commentaires de ce projet.", 'yproject' ); ?></strong>
+					<br><br>
+					<?php _e( "L'acc&egrave;s est r&eacute;serv&eacute; aux investisseurs du projet.", 'yproject' ); ?>
+					<br><br>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- projects/single/template.php
+++ projects/single/template.php
@@ -3,10 +3,24 @@ global $campaign, $can_modify;
 $can_modify = $campaign->current_user_can_edit();
 $client_context = $campaign->get_client_context();
 $campaign_status = $campaign->campaign_status();
+
+if ( is_user_logged_in() ) {
+	$WDGUser_current = WDGUser::current();
+}
+
+// Pour gérer l'affichage de la présentation pour les non-investisseurs après la campagne,
+// On peut voir si :
+// - on peut éditer (PP / admin)
+// - il reste du temps
+// - l'option n'est pas activée
+// - on est connecté et on a investi sur la campagne
+$can_display_presentation = $can_modify
+							|| $campaign->is_remaining_time()
+							|| !$campaign->is_presentation_visible_only_to_investors()
+							|| ( is_user_logged_in() && $WDGUser_current->has_invested_on_campaign( $campaign->ID ) );
 ?>
 
 <?php if ( is_user_logged_in() && $campaign_status == ATCF_Campaign::$campaign_status_vote ): ?>
-	<?php $WDGUser_current = WDGUser::current(); ?>
 	<?php if ( !$WDGUser_current->has_voted_on_campaign( $campaign->ID ) ): ?>
 		<?php locate_template( array("projects/single/voteform-lightbox.php"), true ); ?>
 	<?php else: ?>
@@ -33,15 +47,23 @@ $campaign_status = $campaign->campaign_status();
     </div>
 </header>
 
-<?php locate_template( array("projects/single/nav.php"), true ); ?>
+<?php if ( $can_display_presentation ): ?>
+	<?php locate_template( array("projects/single/nav.php"), true ); ?>
+<?php endif; ?>
 
 <?php locate_template( array("projects/single/rewards.php"), true ); ?>
 
-<?php locate_template( array("projects/single/description.php"), true ); ?>
+<?php if ( $can_display_presentation ): ?>
+	<?php locate_template( array("projects/single/description.php"), true ); ?>
 
-<?php locate_template( array("projects/single/news.php"), true ); ?>
+	<?php locate_template( array("projects/single/news.php"), true ); ?>
 
-<?php locate_template( array("projects/single/comments.php"), true ); ?>
+	<?php locate_template( array("projects/single/comments.php"), true ); ?>
+	
+<?php else: ?>
+	<?php locate_template( array("projects/single/description-hidden.php"), true ); ?>
+
+<?php endif; ?>
 
 <?php 
 $custom_footer_code = $campaign->custom_footer_code();
