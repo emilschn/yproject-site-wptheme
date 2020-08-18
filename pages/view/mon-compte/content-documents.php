892ac8d368eb2b8752421716ab42121ebb7d9982
emilschn <emilien.schneider@poucr.net>
2020-08-18 12:14:02 +0200

    Mon compte : avancées traductions

--- pages/view/mon-compte/content-documents.php
+++ pages/view/mon-compte/content-documents.php
@@ -13,7 +13,7 @@
 <p>
 	<?php _e( 'account.common.INFORMATION_BELOW_PERSONAL_ACCOUNT', 'yproject' ); ?><br>
 	<?php if ( count( $list_current_organizations ) > 0 ): ?>
-		<?php _e( 'account.documents.IF_INVESTMENT_ORGA', 'yproject' ); ?>
+		<?php _e( 'account.common.IF_INVESTMENT_ORGA', 'yproject' ); ?>
 	<?php endif; ?>
 </p>
 
