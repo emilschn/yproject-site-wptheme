6a4adb8b55d7f84feb075c32cfb365abb9db72bd
emilschn <emilien.schneider@poucr.net>
2020-09-04 12:00:28 +0200

    Tunnel investissement : finalisation mise en place traduction

--- pages/view/paiement-effectue/cardwallet-failed.php
+++ pages/view/paiement-effectue/cardwallet-failed.php
@@ -12,10 +12,10 @@ echo $page_controler->get_current_investment()->error_item->get_error_code();
 
 <div class="align-center">
 	<?php if ( $page_controler->get_error_link() != '' ): ?>
-	<a class="button" href="<?php echo $page_controler->get_error_link(); ?>"><?php _e("Suivant", 'yproject'); ?></a>
+	<a class="button" href="<?php echo $page_controler->get_error_link(); ?>"><?php _e( 'common.NEXT', 'yproject' ); ?></a>
 
 	<?php elseif ( $page_controler->get_error_restart_link() != '' ): ?>
-	<a href="<?php echo $page_controler->get_error_restart_link(); ?>"><?php _e( "Red&eacute;marrer l'investissement", 'yproject' ); ?></a>
+	<a href="<?php echo $page_controler->get_error_restart_link(); ?>"><?php _e( 'invest.mean-payment.error.WALLET_TRANSFER_RESTART', 'yproject' ); ?></a>
 
 	<?php endif; ?>
 </div><br><br>
