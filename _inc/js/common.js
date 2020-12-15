684d3e01574367ceff84436d4a62f6b17d14724c
emilschn <emilien.schneider@poucr.net>
2020-12-15 12:34:47 +0100

    Menu : recherche approximative dans les noms de projets et organisations

--- _inc/js/common.js
+++ _inc/js/common.js
@@ -367,6 +367,47 @@ YPUIFunctions = (function($) {
 				}
 			}
 			return "";
+		},
+		
+		getStringsSimilarity: function(s1, s2) {
+			var longer = s1;
+			var shorter = s2;
+			if (s1.length < s2.length) {
+			  longer = s2;
+			  shorter = s1;
+			}
+			var longerLength = longer.length;
+			if (longerLength == 0) {
+			  return 1.0;
+			}
+			var buffer = (longerLength - YPUIFunctions.getStringsDistance(longer, shorter)) / parseFloat(longerLength);
+			return buffer;
+		},
+		getStringsDistance: function(s1, s2) {
+			s1 = s1.toLowerCase();
+			s2 = s2.toLowerCase();
+		  
+			var costs = new Array();
+			for (var i = 0; i <= s1.length; i++) {
+			  var lastValue = i;
+			  for (var j = 0; j <= s2.length; j++) {
+				if (i == 0)
+				  costs[j] = j;
+				else {
+				  if (j > 0) {
+					var newValue = costs[j - 1];
+					if (s1.charAt(i - 1) != s2.charAt(j - 1))
+					  newValue = Math.min(Math.min(newValue, lastValue),
+						costs[j]) + 1;
+					costs[j - 1] = lastValue;
+					lastValue = newValue;
+				  }
+				}
+			  }
+			  if (i > 0)
+				costs[s2.length] = lastValue;
+			}
+			return costs[s2.length];
 		}
 	};
 
@@ -457,11 +498,35 @@ var WDGNavFunctions = (function($) {
 					
 					if (search != "") {
 						var bFoundProject = false;
+						// Découpe par mot recherchés
+						var aSplitSearch = search.split( ' ' );
 						$("#submenu-search .submenu-list li").each(function() {
 							var itemText = $(this).find('a').text().toLowerCase();
 							if (itemText.indexOf(search) > -1) {
 								$(this).removeClass("hidden");
 								bFoundProject = true;
+							}
+							
+							if (!bFoundProject) {
+								var aSplitItem = itemText.split( ' ' );
+								var bFoundInItem = false;
+								for (var i = 0; i < aSplitSearch.length; i++ ) {
+									if ( aSplitSearch[i].length > 2 ) {
+										for (var j = 0; j < aSplitItem.length; j++) {
+											if (aSplitItem[j] != undefined && YPUIFunctions.getStringsSimilarity(aSplitItem[j], aSplitSearch[i]) > 0.7) {
+												bFoundInItem = true;
+												break;
+											}
+										}
+									}
+									if (bFoundInItem) {
+										break;
+									}
+								}
+								if (bFoundInItem) {
+									$(this).removeClass("hidden");
+									bFoundProject = true;
+								}
 							}
 						});
 						if ( !bFoundProject ) {
