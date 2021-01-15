eef3e6a0303cb615f44bccea81d227f29561074a
emilschn <emilien.schneider@poucr.net>
2021-01-15 15:11:03 +0100

    Tunnel investissement : virement : affichage viban

--- pages/view/moyen-de-paiement/wire.php
+++ pages/view/moyen-de-paiement/wire.php
@@ -1,6 +1,7 @@
 <?php
 global $stylesheet_directory_uri;
 $page_controler = WDG_Templates_Engine::instance()->get_controler();
+$viban = $page_controler->get_investor_iban();
 ?>
 
 <div class="center align-justify wdg-lightbox-ref">
@@ -8,20 +9,10 @@ $page_controler = WDG_Templates_Engine::instance()->get_controler();
 
 <?php _e("Afin de proc&eacute;der au virement, voici les informations bancaires dont vous aurez besoin :", 'yproject'); ?><br>
 <ul>
-	<li><strong><?php _e( "Titulaire du compte :", 'yproject' ); ?></strong> LEMON WAY</li>
-	<li><strong>IBAN :</strong> FR76 3000 4025 1100 0111 8625 268</li>
-	<li><strong>BIC :</strong> BNPAFRPPIFE</li>
+	<li><strong><?php _e( "Titulaire du compte :", 'yproject' ); ?></strong> <?php echo $viban->HOLDER; ?></li>
+	<li><strong>IBAN :</strong> <?php echo $viban->IBAN; ?></li>
+	<li><strong>BIC :</strong> <?php echo $viban->BIC; ?></li>
 </ul>
-
-<?php _e( "Code &agrave; indiquer (pour identifier votre paiement) :", 'yproject' ); ?><br>
-<strong><span id="clipboard-user-lw-code">wedogood-<?php echo $page_controler->get_investor_lemonway_id(); ?></span></strong>
-<div class="align-center">
-	<button type="button" class="button blue copy-clipboard" data-clipboard="clipboard-user-lw-code"><?php _e( "Copier le code", 'yproject' ); ?></button>
-	<span class="hidden"><?php _e( "Code copi&eacute;", 'yproject' ); ?></span>
-</div>
-<br><br>
-<?php _e( "Indiquez imp&eacute;rativement ce code comme 'libell&eacute; b&eacute;n&eacute;ficiaire' ou 'code destinataire' au moment du virement !", 'yproject' ); ?>
-
 <br><br>
 
 <div class="db-form full v3">
@@ -33,8 +24,3 @@ $page_controler = WDG_Templates_Engine::instance()->get_controler();
 <br><br>
 
 </div>
-
-
-<hr />
-<?php _e("Exemple de saisie du code destinataire sur diff&eacute;rentes banques :", 'yproject'); ?><br><br>
-<div class="align-center"><img src="<?php echo home_url( '/wp-content/plugins/appthemer-crowdfunding/includes/ui/shortcodes/capture-lbp.png' ); ?>" /></div><br><br>
