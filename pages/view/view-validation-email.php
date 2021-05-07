efb48834d7e898889f1bcd49e6348b6150ca780d
Hélène Meneuvrier <helene@wedogood.co>
2021-05-07 12:18:33 +0200

    WIP Asana 1199943448005334 : début de la page de validation de mail

---
+++ pages/view/view-validation-email.php
@@ -0,0 +1,13 @@
+<?php
+$page_controler = WDG_Templates_Engine::instance()->get_controler();
+?>
+
+<div id="content" class="login-page-container">
+	<div class="padder_more">
+		<div class="center_small margin-height">
+			
+			<h1><?php _e( 'validation.WELCOME_TO_WEDOGOOD', 'yproject' ); ?></h1>
+						
+		</div>
+	</div>
+</div>
