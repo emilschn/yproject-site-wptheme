76818923df7d0c713376566a83f63da74716a778
Lucas Durand <47317956+Lucasdnd@users.noreply.github.com>
2021-05-14 14:20:55 +0200

    Suppression d'un if devenu inutile

--- pages/controler/controler-mon-compte.php
+++ pages/controler/controler-mon-compte.php
@@ -596,9 +596,7 @@ class WDG_Page_Controler_User_Account extends WDG_Page_Controler_WDG {
 	private function init_intentions_to_confirm() {
 		$this->list_intentions_to_confirm = array();
 
-		if ( $this->current_user ) {
-			$this->list_intentions_to_confirm = $this->current_user->get_campaigns_current_voted();
-		}
+		
 	}
 
 	public function get_intentions_to_confirm() {
