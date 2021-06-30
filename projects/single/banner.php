1f372539d77bd4da3e1fa3b86908d8999bed5231
Hélène Meneuvrier <helene@wedogood.co>
2021-06-30 12:15:13 +0200

    Changement de mot

--- projects/single/banner.php
+++ projects/single/banner.php
@@ -362,7 +362,7 @@ $lang_list = $campaign->get_lang_list();
 						<?php else: ?>
 							<div class="end-sentence">
 								<?php if ( $campaign->maximum_complete_message() == '' ): ?>
-									<?php _e( "Seuil maximum atteint: il n'est plus possible d'investir !", 'yproject' ); ?>
+									<?php _e( "Objectif maximum atteint: il n'est plus possible d'investir !", 'yproject' ); ?>
 								<?php else: ?>
 									<?php echo $campaign->maximum_complete_message(); ?>
 								<?php endif; ?>
