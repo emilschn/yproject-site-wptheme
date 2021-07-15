7df1f0f5a0c100d68c073859955d2800320f5cdf
Hélène Meneuvrier <helene@wedogood.co>
2021-07-15 15:02:27 +0200

    Asana 1200609536483451 : changement de l'adresse pour les requêtes custom ajax

--- pages/view/view-connexion.php
+++ pages/view/view-connexion.php
@@ -3,6 +3,6 @@
   id="app"
   data-locale='<?php echo $page_controler->get_init_locale(); ?>'
   data-ajaxurl='<?php echo admin_url( 'admin-ajax.php' ); ?>'
-  data-customajaxurl='<?php echo home_url( '/wp-content/plugins/appthemer-crowdfunding/includes/control/requests/ajax-entry-point.php' ); ?>'
+  data-customajaxurl='<?php echo site_url( '/wp-content/plugins/appthemer-crowdfunding/includes/control/requests/ajax-entry-point.php' ); ?>'
   data-redirecturl='<?php echo wp_unslash( WDGUser::get_login_redirect_page() ); ?>'>
 </div>
