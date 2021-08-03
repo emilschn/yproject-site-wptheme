3593f057478cca7e5f1bbce59c37d413b7ec959e
Lucas Durand <47317956+Lucasdnd@users.noreply.github.com>
2021-08-03 16:56:34 +0200

    Gestion cas de succes et d'erreur

--- pages/view/mon-compte/content-subscription.php
+++ pages/view/mon-compte/content-subscription.php
@@ -1,7 +1,9 @@
 <?php
-global $stylesheet_directory_uri;
-$page_controler = WDG_Templates_Engine::instance()->get_controler();
-$WDGUser_displayed = $page_controler->get_current_user();
+    global $stylesheet_directory_uri;
+    $page_controler = WDG_Templates_Engine::instance()->get_controler();
+    $WDGSubscriptionForm = $page_controler->get_subscription_form();
+    $WDGUser_displayed = $page_controler->get_current_user();
+    $form_feedback = $page_controler->get_user_form_feedback(); 
 ?>
 <h2><?php _e( 'account.subscriptions.SUBSCRIPTIONS_OF', 'yproject' ); ?> <?php echo $page_controler->get_user_name(); ?></h2>
 
@@ -11,7 +13,19 @@ $WDGUser_displayed = $page_controler->get_current_user();
 </p>
 
 <div class="db-form v3 ">
-    <a class="button red add-subscription">
+<?php if ( !empty( $form_feedback[ 'errors' ] ) ): ?>
+	    <div class="wdg-message error">
+        <?php _e( 'form.subscription.error.AMOUNT_MINIMUM', 'yproject' ); ?>
+		</div>
+<?php endif; ?>
+
+<?php if ( !empty( $form_feedback[ 'success' ] ) ): ?>
+        <div class="wdg-message confirm">
+        <?php _e( 'form.subscription.SUCCESS', 'yproject' ); ?>
+        </div>
+<?php endif; ?>
+
+<a class="button red add-subscription">
 	    <span class="button-text">
 	        <?php _e( 'account.subscriptions.ADD_SUBSCRIPTIONS', 'yproject' ); ?>
 	    </span>
