7b035de0c4653f99cd6e9f0332bff196ca17b0df
emilschn <emilien.schneider@poucr.net>
2021-11-16 15:19:15 +0100

    Mon compte : affichage feedback en cas d'erreur d'envoi de fichier

--- pages/view/common/form-orga-identitydocs.php
+++ pages/view/common/form-orga-identitydocs.php
@@ -6,6 +6,7 @@
 	$fields_hidden = $WDGOrganizationIdentityDocsForm->getFields( WDG_Form_User_Identity_Docs::$field_group_hidden );
 	$fields_files = $WDGOrganizationIdentityDocsForm->getFields( WDG_Form_User_Identity_Docs::$field_group_files );
 	$fields_files_orga = $WDGOrganizationIdentityDocsForm->getFields( WDG_Form_User_Identity_Docs::$field_group_files_orga );
+	$form_errors = $WDGOrganizationIdentityDocsForm->get_form_errors();
 ?>
 
 <form method="POST" enctype="multipart/form-data" class="<?php echo $page_controler->get_form_css_classes();?> account-form" action="<?php echo admin_url( 'admin-post.php?action=user_account_organization_identitydocs' ); ?>">
@@ -22,6 +23,15 @@
 			<?php _e( 'account.identitydocs.SOME_FILES_DOUBLE', 'yproject' ); ?><br>
 			<?php foreach ( $kyc_duplicates as $str_duplicate ): ?>
 				- <?php echo $str_duplicate; ?><br>
+			<?php endforeach; ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if ( $WDGUserIdentityDocsForm->hasErrors() ): ?>
+		<div class="wdg-message error">
+			<?php $form_errors = $WDGUserIdentityDocsForm->getPostErrors(); ?>
+			<?php foreach ( $form_errors as $form_error ): ?>
+				<?php echo $form_error[ 'text' ]; ?>
 			<?php endforeach; ?>
 		</div>
 	<?php endif; ?>
