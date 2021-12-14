fa636354f95a7cff47a324e5fc5f8af40a8b87ff
Hélène Meneuvrier <helene@wedogood.co>
2021-12-14 11:30:05 +0100

    retour PR

--- pages/view/tableau-de-bord/tab-documents.php
+++ pages/view/tableau-de-bord/tab-documents.php
@@ -122,9 +122,10 @@ $today_date = new DateTime();
 
 		<?php if ( $page_controler->get_campaign()->campaign_status() == ATCF_Campaign::$campaign_status_closed ): ?>
 			<div class="field admin-theme">
-				<?php _e( "En cas de recouvrement", 'yproject' ); ?>
-				<a href="<?php echo $page_controler->get_campaign()->get_recover_certificate(); ?>?time=<?php echo time(); ?>" download="debt-certificate.pdf" class="button admin-theme"><?php _e( "Attestation de cr&eacute;ance", 'yproject' ); ?></a>
-				<a href="<?php echo $page_controler->get_campaign()->get_recover_list(); ?>?time=<?php echo time(); ?>" download="investors-list.csv" class="button admin-theme"><?php _e( "Liste de l'&eacute;tat des versements", 'yproject' ); ?></a>
+				<?php _e( "En cas de recouvrement", 'yproject' ); ?><br><br>
+				<?php $campaign_debt_files = new WDGCampaignDebtFiles( $page_controler->get_campaign() ); ?>
+				<a href="<?php echo $campaign_debt_files->get_recover_certificate(); ?>?time=<?php echo time(); ?>" download="debt-certificate.pdf" class="button admin-theme"><?php _e( "Attestation de cr&eacute;ance", 'yproject' ); ?></a>
+				<a href="<?php echo $campaign_debt_files->get_recover_list(); ?>?time=<?php echo time(); ?>" download="investors-list.csv" class="button admin-theme"><?php _e( "Liste de l'&eacute;tat des versements", 'yproject' ); ?></a>
 			</div>
 		<?php endif; ?>
 
