c2ed7450f48f1c7384d5a97b93a803c36e748368
emilschn <emilien.schneider@poucr.net>
2022-04-27 11:45:25 +0200

    Popup ajout de vidéo : précision pour les actualités

--- _inc/js/tinymce/video-plugin.js
+++ _inc/js/tinymce/video-plugin.js
@@ -1,22 +1,22 @@
-(function() {
-    tinymce.create('tinymce.plugins.video', {
-        codeUrlVideo: null,
-        init : function(ed, url) {
-            var t = this;
-          
-          //on ajoute le bouton ainsi que son picto
-            ed.addButton('video', {
-                title : 'Ajouter une vidéo',
-                image : url + '/img/mon_picto.png',
-                onclick : function() {
-                    var urlVideo = prompt('Lien de votre vidéo (ceci ajoutera un code spécial, veuillez rafraichir la page pour la visualiser)', '');
-                    if ( urlVideo != null && urlVideo != '') {
-                        codeUrlVideo = '[embed]'+urlVideo+'[/embed]';
-                        ed.execCommand('mceInsertContent', false, codeUrlVideo);    
-                    }
-                }
-            });  
-        },   
-   });
-   tinymce.PluginManager.add('video', tinymce.plugins.video);
+(function () {
+	tinymce.create('tinymce.plugins.video', {
+		codeUrlVideo: null,
+		init: function (ed, url) {
+			var t = this;
+
+			//on ajoute le bouton ainsi que son picto
+			ed.addButton('video', {
+				title: 'Ajouter une vidéo',
+				image: url + '/img/mon_picto.png',
+				onclick: function () {
+					var urlVideo = prompt('Lien de votre vidéo (ceci ajoutera un code spécial, veuillez rafraichir la page pour la visualiser ; si la vidéo est ajoutée dans une actualité, celle-ci ne sera pas affichée dans un e-mail, utilisez plutôt une image)', '');
+					if (urlVideo != null && urlVideo != '') {
+						codeUrlVideo = '[embed]' + urlVideo + '[/embed]';
+						ed.execCommand('mceInsertContent', false, codeUrlVideo);
+					}
+				}
+			});
+		},
+	});
+	tinymce.PluginManager.add('video', tinymce.plugins.video);
 })();
